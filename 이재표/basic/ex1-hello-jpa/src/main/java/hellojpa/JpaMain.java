0ad8ab472ece0e00caf7055e2b5c7e4d68083fd1
jaepyo99 <jaepyo99@naver.com>
2023-09-15 18:08:50 +0900

    [Week1][Chap3] Feat : JPQL소개

--- 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,32 +4,21 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         //엔티티 매니저 팩토리 생성
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        //영속상태가 아닌 단순히 객체에 데이터 주입
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("회원1");
-        //------ 이때까진 비영속 상태 ------
 
         //엔티티 매니저 생성
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
 
-        //객체를 저장한 상태 -> 영속상태
-        em.persist(member);
-        // em.find(원하는 객체,pk값) : 조회
+        //JPQL을 통한 쿼리 작성
+        List<Member> resultList = em.createQuery("select m from Member as m", Member.class).getResultList();
+        System.out.println("결과크기 : "+resultList.size());
 
-        // em.remove(조회한 객체) : 삭제
-
-        /*조회한 객체의 데이터만을 수정하면 트랜잭션을 커밋하는 시점에 수정한다.
-        Member findMember = em.find(Member.class, 1L);
-        findMember.setName("helloB");*/
-
-        //트랜잭션 커밋
         tx.commit();
 
         em.close();
