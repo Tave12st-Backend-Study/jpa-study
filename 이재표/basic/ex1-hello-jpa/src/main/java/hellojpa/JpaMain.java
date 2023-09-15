912318d4ff3ad5b1d998d67c5a2f89d8180731d1
jaepyo99 <jaepyo99@naver.com>
2023-09-15 18:25:46 +0900

    [Week1][Chap3] Feat : 더티체킹

--- 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -13,14 +13,11 @@ public class JpaMain {
 
         EntityManager em = emf.createEntityManager();
         EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
 
-        //엔티티 매니저는 데이터 변경시 트랜잭션을 시작해야 한다.
-        transaction.begin(); // [트랜잭션] 시작
-        Member memberA = new Member(1L, "memberA");
-        Member memberB = new Member(2L, "memberB");
-        em.persist(memberA);
-        em.persist(memberB);
-        //여기까지 INSERT SQL을 데이터베이스에 보내지 않는다.
+        Member member = em.find(Member.class, 1L);
+        member.setName("update");
+        System.out.println("==================");
 
         //커밋하는 순간 데이터베이스에 INSERT SQL을 보낸다.
         transaction.commit(); // [트랜잭션] 커밋
