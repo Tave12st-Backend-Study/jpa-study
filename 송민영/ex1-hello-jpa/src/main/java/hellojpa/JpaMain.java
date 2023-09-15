469c4b7d16e9c871a62f4f5230cb51ca67fa4af0
siss-ankisile <ankisile@gmail.com>
2023-09-16 02:01:34 +0900

    [Week1] [Chap3] Feat: flush

--- 송민영/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 송민영/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,8 +18,9 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = em.find(Member.class, 150L);
-            member.setName("abc");
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+            em.flush();
             System.out.println("==============");
 
             tx.commit();
