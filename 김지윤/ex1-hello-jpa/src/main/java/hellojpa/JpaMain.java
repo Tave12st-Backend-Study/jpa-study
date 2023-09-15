f9859f7194af2e25e2f2fc8dce04d21494ab3a58
jyjyjy25 <jyjyjy7418@gmail.com>
2023-09-16 03:58:34 +0900

    [Week1][Chap3] Feat: 트랜잭션을 지원하는 쓰기 지연

--- 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -13,10 +13,13 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember1 = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L);
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
 
-            System.out.println("result = " + (findMember1 == findMember2));
+            em.persist(member1);
+            em.persist(member2);
+
+            System.out.println("=================");
 
             tx.commit();
         } catch (Exception e) {
