b332c7dfed7add5c75da32fd43f86724f74b30ed
jyjyjy25 <jyjyjy7418@gmail.com>
2023-09-16 04:19:51 +0900

    [Week1][Chap3] Feat: 변경 감지

--- 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -13,11 +13,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
 
-            em.persist(member1);
-            em.persist(member2);
+            // em.persist(member);
 
             System.out.println("=================");
 
