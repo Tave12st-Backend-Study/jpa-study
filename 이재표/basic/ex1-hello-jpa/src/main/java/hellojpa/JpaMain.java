ec238667371b748ef7e9b528cc72716e47926754
jaepyo99 <jaepyo99@naver.com>
2023-09-15 17:55:33 +0900

    [Week][Chap3] Feat : 영속, 비영속 상태
    - 영속상태와 비영속 상태에 대한 코드 작성

--- 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,25 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class JpaMain {
     public static void main(String[] args) {
-        System.out.println("hello jpa");
+        //엔티티 매니저 팩토리 생성
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        //영속상태가 아닌 단순히 객체에 데이터 주입
+        Member member = new Member();
+        member.setName("member1");
+        member.setName("회원1");
+        //------ 이때까진 비영속 상태 ------
+
+        //엔티티 매니저 생성
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        //객체를 저장한 상태 -> 영속상태
+        em.persist(member);
+
     }
 }
