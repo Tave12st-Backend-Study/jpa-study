f61db6fa57ac719b9db0011e6bdc272c1236ba53
jyjyjy25 <jyjyjy7418@gmail.com>
2023-09-16 02:46:53 +0900

    [Week1][Chap2] Feat: 애플리케이션 개발

---
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,35 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(5)
+                    .getResultList();
+
+            for (Member member: result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+}
