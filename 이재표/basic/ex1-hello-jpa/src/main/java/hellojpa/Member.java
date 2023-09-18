8bd0475d2f7e05142cbf7d34ddcd841f33688907
jaepyo99 <jaepyo99@naver.com>
2023-09-19 00:41:17 +0900

    [Week2][Chap4] Feat : 기본키 매핑 init

--- 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -18,13 +18,4 @@ public class Member {
     private Long id;
     @Column(name = "name",nullable = false)
     private String username;
-    private Integer age;
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    @Lob
-    private String description;
 }
