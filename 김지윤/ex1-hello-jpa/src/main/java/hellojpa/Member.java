95d809e541ec8ba8044cefe64908c8927c3733a3
jyjyjy25 <jyjyjy7418@gmail.com>
2023-09-20 01:17:33 +0900

    [Week2][Chap4] Feat: 기본 키 매핑 - TABLE 전략

--- 김지윤/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -6,13 +6,13 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-@SequenceGenerator(
+@TableGenerator(
         name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ",   // 매핑할 데이터베이스 시퀀스 이름
-        initialValue = 1, allocationSize = 50)
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     @Column(name = "name", nullable = false)
