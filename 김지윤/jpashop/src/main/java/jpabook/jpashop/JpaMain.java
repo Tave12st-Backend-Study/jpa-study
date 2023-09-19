311f13e6b07924573707d23328ef599e982dedb5
jyjyjy25 <jyjyjy7418@gmail.com>
2023-09-20 02:14:20 +0900

    [Week2][Chap4] Feat: 실전 예제 1 - 요구사항 분석과 기본 매핑 - RDB 외래키를 통한 조히

--- 김지윤/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ 김지윤/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -17,6 +17,13 @@ public class JpaMain {
         tx.begin();
 
         try {
+            // 주문을 조회한 다음 주문과 연관된 회원을 외래 키를 가지고 조회하기
+            Order order = em.find(Order.class, 1L);
+            Long memberId = order.getMemberId();
+
+            // 외래 키로 다시 조회
+            Member member = em.find(Member.class, memberId);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
