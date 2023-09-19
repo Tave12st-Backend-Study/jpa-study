b6cb5c29c302600eeb0cd28e605f1e7972a201ce
jyjyjy25 <jyjyjy7418@gmail.com>
2023-09-20 02:12:55 +0900

    [Week2][Chap4] Feat: 실전 예제 1 - 요구사항 분석과 기본 매핑 - 프로젝트 셋팅 & 엔티티 생성

---
+++ 김지윤/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+    @Column(name = "MEMBER_ID")
+    private Long MemberId;
+    private LocalDateTime orderDate;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return MemberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        MemberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
