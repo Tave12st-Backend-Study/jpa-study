803d7c19aeb622825db023592a2627a5b56459f4
jaepyo99 <jaepyo99@naver.com>
2023-09-20 22:08:28 +0900

    [Week2][Chap5] Feat : 양방향 매핑 객체세팅

--- 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -3,10 +3,9 @@ package hellojpa;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter @Setter
 @Entity
@@ -15,4 +14,6 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
 }
