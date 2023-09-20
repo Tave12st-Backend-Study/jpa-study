1189a159388e285b183c1c8613d44a829ad17f36
jaepyo99 <jaepyo99@naver.com>
2023-09-20 23:26:49 +0900

    [Week2][Chap5] Feat : 양방향 연관관계시 주인객체

--- 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -20,6 +21,18 @@ public class JpaMain {
             member.setName("member1");
             member.setTeam(team);
             em.persist(member);
+
+            team.getMembers().add(member);
+
+            em.flush();
+            em.clear();
+
+            Team findTeam = em.find(Team.class, team.getId());
+            System.out.println("========");
+            for (Member m : findTeam.getMembers()) {
+                System.out.println("m = "+m.getName());
+            }
+            System.out.println("========");
 
             tx.commit();
         }catch (Exception e){
