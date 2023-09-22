0afd4a88a6219f8487dccdfb92c293514e835e31
siss-ankisile <ankisile@gmail.com>
2023-09-23 00:22:14 +0900

    [Week2][Chap4] Feat: 엔티티 매핑 - Enumerated : EnumType.STRING

--- 송민영/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 송민영/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -10,10 +10,67 @@ public class Member {
     @Column(name = "name", nullable = false, columnDefinition = "varchar(100) default 'EMPTY'")
     private String username;
     private Integer age;
-    @Enumerated
+    @Enumerated(EnumType.STRING)
     private RoleType roleType;
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Temporal(TemporalType.TIMESTAMP)
     private Date lastModifiedDate;
     @Lob
