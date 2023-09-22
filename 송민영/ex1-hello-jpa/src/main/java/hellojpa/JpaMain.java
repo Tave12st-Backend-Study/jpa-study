d076ac32c3aa1e6e95755e8a00cb1639aa149477
siss-ankisile <ankisile@gmail.com>
2023-09-23 02:09:10 +0900

    [Week2][Chap5] Feat: 단방향 연관관계 - 데이터 중심 모델링

--- 송민영/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 송민영/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,20 +18,15 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member1 = new Member();
-            member1.setUsername("A");
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
 
-            Member member2 = new Member();
-            member2.setUsername("B");
+            Member member = new Member();
+            member.setUsername("Member1");
+            member.setTeamId(team.getId());
+            em.persist(member);
 
-            Member member3 = new Member();
-            member3.setUsername("C");
-
-            System.out.println("==========================");
-            em.persist(member1);
-            em.persist(member2);
-            em.persist(member3);
-            System.out.println("==========================");
             tx.commit();
         }
         catch (Exception e){
--- 송민영/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 송민영/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -3,22 +3,17 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
-        initialValue = 1, allocationSize = 1)
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-            generator = "MEMBER_SEQ_GENERATOR")
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name", nullable = false, columnDefinition = "varchar(100) default 'EMPTY'")
+    @Column(name = "USERNAME")
     private String username;
 
-    public Member (){
-
-    }
+    @Column(name = "TEAM_ID")
+    private Long teamId;
 
     public Long getId() {
         return id;
@@ -34,5 +29,13 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
     }
 }
