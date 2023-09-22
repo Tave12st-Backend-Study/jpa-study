4fe7743f5d1a641cc1b491f55ce252309d28158c
siss-ankisile <ankisile@gmail.com>
2023-09-23 00:03:30 +0900

    [Week2][Chap4] Feat: 엔티티 매핑 - 매핑 어노테이션

--- 송민영/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 송민영/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,35 +1,25 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Member {
     @Id
     private Long id;
-    private String name;
+    @Column(name = "name")
+    private String username;
+    private Integer age;
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob
+    private String description;
 
-    public Member() {
-    }
+    public Member (){
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
