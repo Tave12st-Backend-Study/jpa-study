73cbecf1bc485c3338cf4e39967e1b5161f2bada
toychip <dlawnsgud427@naver.com>
2023-09-23 11:20:54 +0900

    [Week2][Chap4] Feat: 실전 예제 - Entity 세팅

--- 임준형/basicjpabook/src/main/java/jpabook/jpashop/domain/Member.java
+++ 임준형/basicjpabook/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,4 +1,21 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
 public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
 }
