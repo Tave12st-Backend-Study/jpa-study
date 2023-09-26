e3f3c978297079b263621cb526c6468850facdd9
Jinu Yi <dionisos198@naver.com>
2023-09-26 17:37:40 +0900

    [Week3][Chap7]Feat:SingleTable 전략

--- 이진우/jpa_basic_jinu/ex1_hello_jpa_dionisos198/src/main/java/jpa_basic_dionisos198/Item.java
+++ 이진우/jpa_basic_jinu/ex1_hello_jpa_dionisos198/src/main/java/jpa_basic_dionisos198/Item.java
@@ -3,7 +3,7 @@ package jpa_basic_dionisos198;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn
 public class Item {
     @Id @GeneratedValue
