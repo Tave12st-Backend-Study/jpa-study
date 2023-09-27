1bf20f98401396f1caec2eeaf2ad2097fd6b02cb
siss-ankisile <ankisile@gmail.com>
2023-09-28 03:38:45 +0900

    [Week3][Chap6] Feat: 실전 예제 3

--- 송민영/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ 송민영/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,6 +17,10 @@ public class Order {
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
+    @OneToOne
+    @JoinColumn(name = "DELIVER_ID")
+    private Delivery delivery;
+
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
     private LocalDateTime orderDate;
