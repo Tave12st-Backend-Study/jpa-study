e6404316a0b35571e783368a09aab4330bbcb32c
siss-ankisile <ankisile@gmail.com>
2023-09-28 03:17:36 +0900

    [Week3][Chap6] Feat: 다대다 양방향

--- 송민영/ex1-hello-jpa/src/main/java/hellojpa/Product.java
+++ 송민영/ex1-hello-jpa/src/main/java/hellojpa/Product.java
@@ -3,6 +3,9 @@ package hellojpa;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Product {
@@ -11,6 +14,9 @@ public class Product {
     private Long id;
 
     private String name;
+
+    @ManyToMany(mappedBy="products")
+    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
