d067030367544ab574d635f001806cc564f823b2
Jinu Yi <dionisos198@naver.com>
2023-09-28 13:41:23 +0900

    [Week3][Chap7]Feat:고급매핑 실전예제

---
+++ 이진우/jpa_basic_jinu/jpa_shop_dionisos198/src/main/java/jpa_book_dionisos198/domain/Album.java
@@ -0,0 +1,25 @@
+package jpa_book_dionisos198.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Album extends Item{
+    String artist;
+    private String etc;
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getEtc() {
+        return etc;
+    }
+
+    public void setEtc(String etc) {
+        this.etc = etc;
+    }
+}
