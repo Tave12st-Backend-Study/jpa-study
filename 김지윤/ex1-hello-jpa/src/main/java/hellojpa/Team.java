d3915d3ed97afe8d18ffe476feed4a6d9ca68dca
jyjyjy25 <jyjyjy7418@gmail.com>
2023-09-28 20:05:50 +0900

    [Week3][Chap6] Feat: 다대일 - 양방향

--- 김지윤/ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -11,6 +11,9 @@ public class Team {
     private Long id;
     private String name;
 
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
     public Long getId() {
         return id;
     }
