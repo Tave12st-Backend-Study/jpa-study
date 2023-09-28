9d375a4abf3aa4bf0b0059ccc7e200fc6f652087
jyjyjy25 <jyjyjy7418@gmail.com>
2023-09-28 21:07:18 +0900

    [Week3][Chap6] Feat: 일대다 - 양방향

--- 김지윤/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -11,6 +11,10 @@ public class Member {
     @Column(name = "USERNAME")
     private String name;
 
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+    private Team team;
+
     public Long getId() {
         return id;
     }
