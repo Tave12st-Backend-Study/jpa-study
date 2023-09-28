128a7ac862fc709b759b6f01f62b08670fda8ab3
toychip <dlawnsgud427@naver.com>
2023-09-28 21:06:57 +0900

    [Week3][Chap6] Feat: 다양한 연관관계 매핑 - 다대일 [N:1] 양방향

--- 임준형/basic/reviewjpa/src/main/java/reviewjpa/Team.java
+++ 임준형/basic/reviewjpa/src/main/java/reviewjpa/Team.java
@@ -17,5 +17,8 @@ public class Team {
     private Long id;
     private String name;
 
+    @OneToMany(mappedBy = "team")
+    // 여기서 team은 Member객체에서 Team을 참조하는 변수 명
+    private List<Member> members = new ArrayList<>();
 
 }
