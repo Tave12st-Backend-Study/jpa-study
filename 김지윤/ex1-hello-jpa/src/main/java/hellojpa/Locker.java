b0b77abfdc409bb0d379a63701b516901a41ac41
jyjyjy25 <jyjyjy7418@gmail.com>
2023-09-28 21:12:07 +0900

    [Week3][Chap6] Feat: 일대일 - 주 테이블에 외래 키 - 단방향

---
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/Locker.java
@@ -0,0 +1,16 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Locker {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+
+}
