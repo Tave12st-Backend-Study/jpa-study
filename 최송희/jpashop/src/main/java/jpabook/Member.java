13482cefecab9f2175f4572acc39524633887a70
songhee <96781855+songhee1@users.noreply.github.com>
2023-09-28 10:25:00 +0900

    [Week][Chat6] Feat : 다대다

--- 최송희/jpashop/src/main/java/jpabook/Member.java
+++ 최송희/jpashop/src/main/java/jpabook/Member.java
@@ -1,6 +1,7 @@
 package jpabook;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.locks.Lock;
 
@@ -19,6 +20,9 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
---
+++ 최송희/jpashop/src/main/java/jpabook/Product.java
@@ -0,0 +1,37 @@
+package jpabook;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Product {
+    @Id @GeneratedValue
+    @Column(name = "PRODUCT_ID")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "product")
+    private List<MemberProduct> memberProductList;
+
+    private int count;
+    private int price;
+    private LocalDateTime orderDateTime;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
