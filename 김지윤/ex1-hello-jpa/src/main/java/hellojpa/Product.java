a2c13e839043ba3b6fa5a2002a3b760be9404607
jyjyjy25 <jyjyjy7418@gmail.com>
2023-09-29 00:26:17 +0900

    [Week3][Chap6] Feat: 다대다 - 양방향

--- 김지윤/ex1-hello-jpa/src/main/java/hellojpa/Product.java
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/Product.java
@@ -1,8 +1,8 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Product {
@@ -10,8 +10,11 @@ public class Product {
     private Long id;
     private String name;
 
+    @ManyToMany(mappedBy = "products")
+    private List<Member> members = new ArrayList<>();
+
     public Long getId() {
-        return id;
+        return id;ㅂㅂ
     }
 
     public void setId(Long id) {
