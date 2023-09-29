32d7bded9d700c534284f9eebe7acb95bcdd826d
jyjyjy25 <jyjyjy7418@gmail.com>
2023-09-30 03:06:01 +0900

    [Week3][Chap7] Feat: Mapped Superclass - 매핑 정보 상속

--- 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -13,19 +14,15 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Movie movie = new Movie();
-            movie.setDirector("aaaa");
-            movie.setActor("bbbb");
-            movie.setName("바람과함께사라지다");
-            movie.setPrice(10000);
+            Member member = new Member();
+            member.setName("user1");
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
 
-            em.persist(movie);
+            em.persist(member);
 
             em.flush();
             em.clear();
-
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
 
             tx.commit();
         } catch (Exception e) {
--- 김지윤/ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Team {
+public class Team extends BaseEntity {
     @Id @GeneratedValue
     @Column(name = "TEAM_ID")
     private Long id;
