8b714a29fd54f19317f15a616fcaa89445a447c7
siss-ankisile <ankisile@gmail.com>
2023-10-01 13:51:43 +0900

    [Week4][Chap8] Feat: 영속성 전이와 고아객체 - 고아객체(#44)

--- 송민영/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 송민영/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -27,6 +27,12 @@ public class JpaMain {
 
             em.persist(parent);
 
+            em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
+
             tx.commit();
         }
         catch (Exception e){
--- 송민영/ex1-hello-jpa/src/main/java/hellojpa/Parent.java
+++ 송민영/ex1-hello-jpa/src/main/java/hellojpa/Parent.java
@@ -12,7 +12,7 @@ public class Parent {
 
     private String name;
 
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Child> childList = new ArrayList<>();
 
     public void addChild(Child child){
@@ -34,5 +34,13 @@ public class Parent {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Child> getChildList() {
+        return childList;
+    }
+
+    public void setChildList(List<Child> childList) {
+        this.childList = childList;
     }
 }
