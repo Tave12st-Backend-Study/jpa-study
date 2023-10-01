5a3bc4752c41795ad4d58262a87a7a76e1013ad4
toychip <dlawnsgud427@naver.com>
2023-10-01 16:44:40 +0900

    [Week4][Chap8] Feat: 프록시 - em.find em.getReference 차이 (#45)

--- 임준형/basic/reviewjpa/src/main/java/reviewjpa/Locker.java
+++ 임준형/basic/reviewjpa/src/main/java/reviewjpa/Locker.java
@@ -1,21 +1,22 @@
-package reviewjpa;
-
-import lombok.Getter;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-
-@Getter
-@Entity
-public class Locker {
-
-    @Id @GeneratedValue
-    private Long id;
-
-    private String name;
-
-    @OneToOne(mappedBy = "locker")
-    private Member member;
-}
+//package reviewjpa;
+//
+//import lombok.Getter;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+//import javax.persistence.OneToOne;
+//
+//@Getter
+//@Entity
+//public class Locker {
+//    // 1대 1 매핑 예제
+//
+//    @Id @GeneratedValue
+//    private Long id;
+//
+//    private String name;
+//
+//    @OneToOne(mappedBy = "locker")
+//    private Member member;
+//}
