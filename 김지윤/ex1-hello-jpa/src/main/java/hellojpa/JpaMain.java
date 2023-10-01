b53c21ecc8ac58ef062a38c6123f797f467556e8
jyjyjy25 <jyjyjy7418@gmail.com>
2023-10-02 03:01:41 +0900

    [Week4][Chap8] Feat: 프록시 - JPA의 동일성 보장

--- 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,17 +18,42 @@ public class JpaMain {
             member1.setName("member1");
             em.persist(member1);
 
-            Member member2 = new Member();
-            member2.setName("member2");
-            em.persist(member2);
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member1.getId()); // 엔티티 객체
+            System.out.println("findMember = " + findMember.getClass());
+
+            Member refMember = em.getReference(Member.class, member1.getId()); // 엔티티 객체
+            System.out.println("refMember = " + refMember.getClass());
+
+            System.out.println("findMember == refMember: " + (findMember == refMember)); // True
 
             em.flush();
             em.clear();
 
-            Member m1 = em.find(Member.class, member1.getId());
-            Member m2 = em.getReference(Member.class, member2.getId());
+            System.out.println("-------------------------");
 
-            logic(m1, m2);
+            Member refMember2 = em.getReference(Member.class, member1.getId()); // 프록시 객체
+            System.out.println("refMember2 = " + refMember2.getClass());
+
+            Member refMember3 = em.getReference(Member.class, member1.getId()); // 프록시 객체
+            System.out.println("refMember3 = " + refMember3.getClass());
+
+            System.out.println("refMember2 == refMember3: " + (refMember2 == refMember3)); // True
+
+            em.flush();
+            em.clear();
+
+            System.out.println("-------------------------");
+
+            Member refMember4 = em.getReference(Member.class, member1.getId()); // 프록시 객체
+            System.out.println("refMember4 = " + refMember4.getClass());
+
+            Member findMember2 = em.find(Member.class, member1.getId()); // 프록시 객체
+            System.out.println("findMember2 = " + findMember2.getClass());
+
+            System.out.println("refMember3 == findMember2: " + (refMember4 == findMember2)); // True
 
             tx.commit();
         } catch (Exception e) {
@@ -38,7 +63,6 @@ public class JpaMain {
         }
 
         emf.close();
-
     }
 
     private static void logic(Member m1, Member m2) {
