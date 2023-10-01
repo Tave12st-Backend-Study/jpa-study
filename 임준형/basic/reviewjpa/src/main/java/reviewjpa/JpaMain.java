4d6972eb0370506b98b6c79daaee1afef6b83d2a
toychip <dlawnsgud427@naver.com>
2023-10-01 18:20:34 +0900

    [Week4][Chap8] Feat: JPQL N+1 문제 (#45)

--- 임준형/basic/reviewjpa/src/main/java/reviewjpa/JpaMain.java
+++ 임준형/basic/reviewjpa/src/main/java/reviewjpa/JpaMain.java
@@ -23,14 +23,24 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("TeamA");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("TeamB");
+            em.persist(teamB);
 
             Member member1 = new Member();
             member1.setUsername("member1");
-            member1.setTeam(team);
+            member1.setTeam(teamA);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(teamB);
+            em.persist(member2);
+
 
             em.flush();
             em.clear();
@@ -40,16 +50,23 @@ public class JpaMain {
             System.out.println("----- 한번에 쿼리가 나감 -----");
             System.out.println("----- 하지만 실무에서는 가급적 지연 로딩만 사용한다. -----");
 
-
+            System.out.println("----- em.find는 pk를 찍어서 가져오기 때문에 JPA가 내부적으로 최적화 가능하다.");
             Member member = em.find(Member.class, member1.getId());
-            System.out.println(" flag1 ----- ----- ");
-            Team getTeam = member.getTeam();    // proxy를 가져오기 때문에 어떠한 쿼리도 나가지 않음
-            System.out.println(" flag2 ----- ----- ");
-            System.out.println("getTeam.getClass() = " + getTeam.getClass());   // proxy
 
-            System.out.println(" flag3 ----- ----- ");
-            System.out.println("getTeam.getName() = " + getTeam.getName());
+            System.out.println("----- JPQL은 우선 SQL로 번역이된다. ----- ");
+            System.out.println("----- Member를 전체 조회하는 쿼리인데, 즉시로딩이므로 Team의 값이 다 들어가있어야한다. -----");
+            List<Member> selectMFromMemberM =
+                    em.createQuery("select m from Member m", Member.class).getResultList();
+            System.out.println("----- SQL: select * " +
+                                            "from Member" +
+                                            " -----");
+            System.out.println("----- 이렇게 가져온 후, Member가 즉시로딩이므로 Team 조회하는 쿼리 출력 -----");
+            System.out.println("----- SQL: select * " +
+                                            "from Team " +
+                                           "Where TEAM_ID = MEMBER_ID -----");
 
+            System.out.println("----- team이 지금 2개인데, member를 전체조회하는 쿼리 1개와, " +
+                    "각 team마다 조회하는 쿼리 n개가 나오기 때문에 n+1 문제라고한다. -----");
 
             tx.commit(); // 성공하면 커밋
 
