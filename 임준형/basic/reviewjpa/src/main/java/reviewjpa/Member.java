dc13ffda18b1fd713079b606032c62129fedb088
toychip <dlawnsgud427@naver.com>
2023-10-01 18:27:14 +0900

    [Week4][Chap8] Feat: JPQL N+1 문제 해결 (#45)

--- 임준형/basic/reviewjpa/src/main/java/reviewjpa/Member.java
+++ 임준형/basic/reviewjpa/src/main/java/reviewjpa/Member.java
@@ -16,7 +16,7 @@ public class Member extends BaseEntity {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
