fbd9c1a6e12eb04afb1d2baabeb94a986c266412
toychip <dlawnsgud427@naver.com>
2023-10-01 18:27:14 +0900

    [Week4][Chap8] Feat: JPQL N+1 문제 해결 (#45)

--- 임준형/basic/reviewjpa/src/main/java/reviewjpa/JpaMain.java
+++ 임준형/basic/reviewjpa/src/main/java/reviewjpa/JpaMain.java
@@ -45,28 +45,14 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            System.out.println("----- 만약 member와 team이 90프로 이상 같이 사용한다면 EAGER를 사용할것 -----");
-            System.out.println("----- 프록시 객체가 존재하지 않기 때문에 초기화 과정 필요 없음 -----");
-            System.out.println("----- 한번에 쿼리가 나감 -----");
-            System.out.println("----- 하지만 실무에서는 가급적 지연 로딩만 사용한다. -----");
+           // Member member = em.find(Member.class, member1.getId());
 
-            System.out.println("----- em.find는 pk를 찍어서 가져오기 때문에 JPA가 내부적으로 최적화 가능하다.");
-            Member member = em.find(Member.class, member1.getId());
-
-            System.out.println("----- JPQL은 우선 SQL로 번역이된다. ----- ");
-            System.out.println("----- Member를 전체 조회하는 쿼리인데, 즉시로딩이므로 Team의 값이 다 들어가있어야한다. -----");
+            // fetch join
             List<Member> selectMFromMemberM =
-                    em.createQuery("select m from Member m", Member.class).getResultList();
-            System.out.println("----- SQL: select * " +
-                                            "from Member" +
-                                            " -----");
-            System.out.println("----- 이렇게 가져온 후, Member가 즉시로딩이므로 Team 조회하는 쿼리 출력 -----");
-            System.out.println("----- SQL: select * " +
-                                            "from Team " +
-                                           "Where TEAM_ID = MEMBER_ID -----");
+                    em.createQuery("select m from Member m join fetch m.team", Member.class).getResultList();
 
-            System.out.println("----- team이 지금 2개인데, member를 전체조회하는 쿼리 1개와, " +
-                    "각 team마다 조회하는 쿼리 n개가 나오기 때문에 n+1 문제라고한다. -----");
+            System.out.println("----- fetch join을 했기 때문에 값이 다 채워져서 나온다. -----");
+            System.out.println("----- @EntityGraph or Batch size를 활용하여 해결할 수 있지만 대부분 fetch join으로 해결함 -----");
 
             tx.commit(); // 성공하면 커밋
 
