8bb891548f2fc81aa1bb1d5b34756ab730ceb8af
songhee <96781855+songhee1@users.noreply.github.com>
2023-10-01 14:56:13 +0900

    [Week][Chat8] Feat : 프록시 - 영속성 컨텍스트의 관리를 벗어나는 경우 (#48)

--- 최송희/jpashop/src/main/java/jpabook/Main.java
+++ 최송희/jpashop/src/main/java/jpabook/Main.java
@@ -22,26 +22,22 @@ public class Main {
             member1.setUserName("hello");
             em.persist(member1);
 
-            Member member2 = new Member();
-            member2.setUserName("hello");
-            em.persist(member2);
-
             em.flush();
             em.clear();
 
             Member m1Reference = em.getReference(Member.class, member1.getId());
             System.out.println("m1 = :"+m1Reference.getClass());//Proxy
 
+//            em.detach(m1Reference);//영속성 컨텍스트에서 관리 안해!
+            em.close();
 
-            Member m2 = em.getReference(Member.class, member1.getId());
-            System.out.println("m2 = :"+m2.getClass());//Proxy
-
-            System.out.println("m1Referece == m2 : "+ (m1Reference == m2));
+            m1Reference.getUserName();//SELECT 쿼리 발생(영속성 컨텍스트의 도움을 받아서)
 
 
             tx.commit();
         }catch(Exception e){
             tx.rollback();
+            e.printStackTrace();
         }finally {
             em.close();
         }
