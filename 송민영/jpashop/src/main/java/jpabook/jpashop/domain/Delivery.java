f70ad5cee25c2a71d426de9c61048bf001a6ed9b
siss-ankisile <ankisile@gmail.com>
2023-10-01 13:58:50 +0900

    [Week4][Chap8] Feat: 실전 예제 5-연관관계 관리(#44)

--- 송민영/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ 송민영/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 public class Delivery extends BaseEntity {
 
@@ -17,6 +19,6 @@ public class Delivery extends BaseEntity {
     private String zipcode;
     private DeliveryStatus status;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
 }
--- 송민영/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ 송민영/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.domain;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 public class OrderItem extends BaseEntity {
     @Id
@@ -9,11 +11,11 @@ public class OrderItem extends BaseEntity {
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "ORDER_ID")
     private Order order;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "ITEM_ID")
     private Item item;
 
