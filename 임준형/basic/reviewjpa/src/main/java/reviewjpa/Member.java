2d511dfe38f947eccd0e68cd40503cde519a10de
toychip <dlawnsgud427@naver.com>
2023-10-01 18:00:54 +0900

    [Week4][Chap8] Feat: 지연로딩 FetchType.LAZY (#45)

--- 임준형/basic/reviewjpa/src/main/java/reviewjpa/Member.java
+++ 임준형/basic/reviewjpa/src/main/java/reviewjpa/Member.java
@@ -16,7 +16,7 @@ public class Member extends BaseEntity {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
