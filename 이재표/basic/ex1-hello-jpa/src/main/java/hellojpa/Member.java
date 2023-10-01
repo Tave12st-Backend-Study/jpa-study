c157e6e513331ca83f0cca371a96beb23c118c84
jaepyo99 <jaepyo99@naver.com>
2023-10-02 00:17:03 +0900

    [Week4][Chapter8] Feat : 즉시로딩

--- 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -21,7 +21,7 @@ public class Member extends BaseEntity {
     private Long id;
     private String name;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
