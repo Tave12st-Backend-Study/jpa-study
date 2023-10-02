6035aca6b8a5c7e35d021bdf4a25f1cd2febfb59
jaepyo99 <jaepyo99@naver.com>
2023-10-02 18:21:55 +0900

    [Week4][Chapter9] Feat : 값타입 비교 (#47)

--- 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/Address.java
+++ 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/Address.java
@@ -8,13 +8,30 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Embeddable
-@Getter @Setter
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city)
+                && Objects.equals(street, address.street)
+                && Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
+    }
 }
--- 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
+++ 이재표/basic/ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
@@ -2,14 +2,11 @@ package hellojpa;
 
 public class ValueMain {
     public static void main(String[] args) {
-//        int a = 10;
-//        int b = a;
-//        a=20;
-
-        Integer a = new Integer(10);
-        Integer b = a;
-
-        System.out.println("a = " + a);
-        System.out.println("b = " + a);
+        int a = 10;
+        int b = 10;
+        System.out.println("a == b : "+(a==b));
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
+        System.out.println("address1 equals address2 : "+(address2.equals(address1)));
     }
 }
