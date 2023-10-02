c0925f5af1003aee1dc3ac312b16f26b137b5e37
siss-ankisile <ankisile@gmail.com>
2023-10-02 18:15:33 +0900

    [Week4][Chap9] Feat: @AttributeOverride: 속성 재정의 (#44)

--- 송민영/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 송민영/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -19,6 +19,14 @@ public class Member {
     @Embedded
     private Address homeAddress;
 
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="city", column = @Column(name= "WORK_CITY")),
+            @AttributeOverride(name="street", column = @Column(name= "WORK_STREET")),
+            @AttributeOverride(name="zipcode", column = @Column(name= "WORK_ZIPCODE"))
+    })
+    private Address workAddress;
+
     public Long getId() {
         return id;
     }
