6f9dab167f8f227b8f299ca9d33857875ffac74b
jyjyjy25 <jyjyjy7418@gmail.com>
2023-10-03 19:31:11 +0900

    [Week4][Chap9] Feat: 값 타입과 불변 객체 - 불변 객체 (#49)

--- 김지윤/ex1-hello-jpa/src/main/java/hellojpa/Address.java
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/Address.java
@@ -21,23 +21,11 @@ public class Address {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public String getStreet() {
         return street;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
     public String getZipcode() {
         return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
     }
 }
--- 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -23,14 +23,8 @@ public class JpaMain {
             member1.setHomeAddress(address);
             em.persist(member1);
 
-            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
-
-            Member member2 = new Member();
-            member2.setName("member2");
-            member2.setHomeAddress(copyAddress);
-            em.persist(member2);
-
-            member1.getHomeAddress().setCity("newCity");
+            Address newAddress = new Address("newCity", address.getStreet(), address.getZipcode());
+            member1.setHomeAddress(newAddress);
 
             tx.commit();
         } catch (Exception e) {
