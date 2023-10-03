10898156f7af52cb804a4aaebfb35b3245c60642
jyjyjy25 <jyjyjy7418@gmail.com>
2023-10-04 01:10:46 +0900

    [Week4][Chap9] Feat: 실전 예제 6 - 값 타입 매핑 (#49)

--- 김지윤/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ 김지윤/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -6,11 +6,32 @@ import javax.persistence.*;
 public class Delivery extends BaseEntity {
     @Id @GeneratedValue
     private Long id;
-    private String city;
-    private String street;
-    private String zipcode;
-    private DeliveryStatus status;
+    private Address address;
 
     @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
