4960745122f7f921b9a6fb72d4de1ea7e676cef2
jyjyjy25 <jyjyjy7418@gmail.com>
2023-10-04 00:25:01 +0900

    [Week4][Chap9] Feat: 값 타입 컬렉션 - 값 타입 컬렉션 저장/조회/수정 (#49)

--- 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
 
@@ -15,16 +16,51 @@ public class JpaMain {
         tx.begin();
 
         try {
+            //* 값 타입 컬렉션 저장
+            Member member = new Member();
+            member.setName("member1");
+            member.setHomeAddress(new Address("homecity", "street", "10000"));
 
-            Address address = new Address("city", "street", "10000");
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("피자");
+            member.getFavoriteFoods().add("족발");
 
-            Member member1 = new Member();
-            member1.setName("member1");
-            member1.setHomeAddress(address);
-            em.persist(member1);
+            member.getAddressHistory().add(new Address("old1", "street", "10000"));
+            member.getAddressHistory().add(new Address("old2", "street", "10000"));
 
-            Address newAddress = new Address("newCity", address.getStreet(), address.getZipcode());
-            member1.setHomeAddress(newAddress);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            //* 값 타입 컬렉션 조회
+            System.out.println("========== START ==========");
+            Member findMember = em.find(Member.class, member.getId());
+
+            List<Address> addressHistory = findMember.getAddressHistory();
+            for (Address address : addressHistory) {
+                System.out.println("address = " + address.getCity());
+            }
+
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            for (String favoriteFood : favoriteFoods) {
+                System.out.println("favoriteFood = " + favoriteFood);
+            }
+
+            //* 값 타입 컬렉션 수정
+            System.out.println("========== START ==========");
+            Address a = findMember.getHomeAddress();
+            // 임베디드값 타입 수정, homeCity -> newCity
+            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
+
+            // 기본값 타입 컬렉션 수정, 치킨 -> 한식
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
+
+            // 임베디드값 타입 컬렉션 수정, old1 -> newCity1
+            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
+            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
+
 
             tx.commit();
         } catch (Exception e) {
--- 김지윤/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,6 +2,10 @@ package hellojpa;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member {
@@ -13,19 +17,20 @@ public class Member {
     private String name;
 
     @Embedded
-    private Period workPeriod;
-
-    @Embedded
     private Address homeAddress;
 
-    @Embedded
-    @AttributeOverrides({
-            @AttributeOverride(name="city", column=@Column(name = "WORK_CITY")),
-            @AttributeOverride(name="street", column=@Column(name = "WORK_STREET")),
-            @AttributeOverride(name="zipcode", column=@Column(name = "WORK_ZIPCODE")),
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
 
-    })
-    private Address workAddress;
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    private List<Address> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -43,19 +48,27 @@ public class Member {
         this.name = name;
     }
 
-    public Period getWorkPeriod() {
-        return workPeriod;
-    }
-
-    public void setWorkPeriod(Period workPeriod) {
-        this.workPeriod = workPeriod;
-    }
-
     public Address getHomeAddress() {
         return homeAddress;
     }
 
     public void setHomeAddress(Address homeAddress) {
         this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<Address> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<Address> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
