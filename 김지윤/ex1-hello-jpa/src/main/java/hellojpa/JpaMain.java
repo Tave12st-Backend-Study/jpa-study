81a675d5d2a38a8c9a1bec0b1197995ed79cdf42
jyjyjy25 <jyjyjy7418@gmail.com>
2023-10-03 19:09:31 +0900

    [Week4][Chap9] Feat: 값 타입과 불변 객체 - 값 타입의 공유 참조 문제 (#49)

--- 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,12 +15,20 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setName("hello");
-            member.setHomeAddress(new Address("city", "street", "100"));
-            member.setWorkPeriod(new Period());
 
-            em.persist(member);
+            Address address = new Address("city", "street", "10000");
+
+            Member member1 = new Member();
+            member1.setName("member1");
+            member1.setHomeAddress(address);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setName("member2");
+            member2.setHomeAddress(address);
+            em.persist(member2);
+
+            member1.getHomeAddress().setCity("newCity");
 
             tx.commit();
         } catch (Exception e) {
