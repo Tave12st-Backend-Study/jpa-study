8b79c528a676e15387eef8b565e71bc9aadf2776
songhee <96781855+songhee1@users.noreply.github.com>
2023-10-04 18:50:01 +0900

    [Week][Chat8] Feat : 지연로딩, 자식객체 자동 삭제 설정 ( #48)

--- 최송희/jpashop/src/main/java/jpabook/Category.java
+++ 최송희/jpashop/src/main/java/jpabook/Category.java
@@ -12,7 +12,7 @@ public class Category extends BaseEntity{
     @Column(name = "CATEGORY_ID")
     private Long id;
     private String name;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "PARENT_ID")
     private Category parent;
 
--- 최송희/jpashop/src/main/java/jpabook/OrderItem.java
+++ 최송희/jpashop/src/main/java/jpabook/OrderItem.java
@@ -8,10 +8,10 @@ public class OrderItem {
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ORDER_ID")
     private Order order;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ITEM_ID")
     private Item item;
     private int orderPrice;
