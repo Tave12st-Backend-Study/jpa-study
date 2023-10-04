7543925545b850c0a99dc8032338dccd0eb21898
toychip <dlawnsgud427@naver.com>
2023-10-04 19:48:52 +0900

    [Week4][Chap9] Feat: 실전예제6 값 타입 Embedded 매핑 (#45)

--- 임준형/basicjpabook/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ 임준형/basicjpabook/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -9,12 +9,11 @@ public class Delivery extends BaseEntity{
     @Id @GeneratedValue
     private Long id;
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
+
     private DeliveryStatus status;
 
-    @OneToOne(fetch = LAZY,
-            mappedBy = "delivery")
+    @OneToOne(fetch = LAZY, mappedBy = "delivery")
     private Order order;
 }
--- 임준형/basicjpabook/src/main/java/jpabook/jpashop/domain/Member.java
+++ 임준형/basicjpabook/src/main/java/jpabook/jpashop/domain/Member.java
@@ -17,9 +17,9 @@ public class Member extends BaseEntity{
     @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded
+    private Address address;
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
