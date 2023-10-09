8d8bd9890ee6e2b4da8c4f5889768c850feedb47
toychip <dlawnsgud427@naver.com>
2023-10-09 22:39:45 +0900

    [Week5][Chap10] Feat: TypeQuery, Query (#57)

--- 임준형/jpqlpractice/src/main/java/jpql/JpaMain.java
+++ 임준형/jpqlpractice/src/main/java/jpql/JpaMain.java
@@ -1,9 +1,6 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,7 +12,20 @@ public class JpaMain {
         tx.begin(); // transaction 시작
 
         try {
-            
+
+            Member member = new Member();
+            member.setUsername("newnew");
+            member.setAge(10);
+            em.persist(member);
+
+            // 제너릭으로 같고 있음
+            TypedQuery<Member> selectMFromMemberM = em.createQuery("select m from Member m", Member.class);
+            System.out.println("----- 반환 타입이 string이면 아래와 같이 가능 -----");
+            TypedQuery<String> stringQuery = em.createQuery("select m.username from Member m", String.class);
+
+            System.out.println("----- Type 정보가 string과 int로 복합적이므로, 이 때 (반환 타입이 명확하지 않을 때)는 Query로 해야함. -----");
+            Query objectQuery = em.createQuery("select m.username, m.age from Member m", String.class);
+
             tx.commit(); // 성공하면 커밋
 
         } catch (Exception e) {
