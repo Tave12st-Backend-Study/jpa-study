c8bc7ff85b7943849e9277415ec1ba9e8a906c6f
toychip <dlawnsgud427@naver.com>
2023-10-09 22:43:52 +0900

    [Week5][Chap10] Feat: getResultList(), getSingleResult (#57)

--- 임준형/jpqlpractice/src/main/java/jpql/JpaMain.java
+++ 임준형/jpqlpractice/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -19,12 +20,17 @@ public class JpaMain {
             em.persist(member);
 
             // 제너릭으로 같고 있음
-            TypedQuery<Member> selectMFromMemberM = em.createQuery("select m from Member m", Member.class);
-            System.out.println("----- 반환 타입이 string이면 아래와 같이 가능 -----");
-            TypedQuery<String> stringQuery = em.createQuery("select m.username from Member m", String.class);
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
 
-            System.out.println("----- Type 정보가 string과 int로 복합적이므로, 이 때 (반환 타입이 명확하지 않을 때)는 Query로 해야함. -----");
-            Query objectQuery = em.createQuery("select m.username, m.age from Member m", String.class);
+            List<Member> resultList = query.getResultList();
+
+            for (Member m : resultList) {
+                System.out.println("m.getUsername() = " + m.getUsername());
+            }
+
+            Member singleMember = query.getSingleResult();
+            System.out.println("singleMember.getUsername() = " + singleMember.getUsername());
+
 
             tx.commit(); // 성공하면 커밋
 
