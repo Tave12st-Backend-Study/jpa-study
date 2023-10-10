41ee997f6f20001d9691cbbfffa3b7b5330ebcd5
Jinu Yi <dionisos198@naver.com>
2023-10-10 12:19:43 +0900

    [Week5][Chap10] Feat : Criteria 소개 (#59)

--- 이진우/jpa_basic_jinu/ex1_hello_jpa_dionisos198/src/main/java/jpa_basic_dionisos198/JpaMain.java
+++ 이진우/jpa_basic_jinu/ex1_hello_jpa_dionisos198/src/main/java/jpa_basic_dionisos198/JpaMain.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
@@ -15,13 +18,11 @@ public class JpaMain {
         EntityTransaction tx=em.getTransaction();
         tx.begin();
         try{
-            String jpql="select m from Member m where m.name like '%kim%'";
-            List<Member> result=em.createQuery(jpql,Member.class).getResultList();
-
-           /* for (Member member : result) {
-                System.out.println(member.getName());
-            }*/
-
+            CriteriaBuilder cb=em.getCriteriaBuilder();
+            CriteriaQuery<Member> query=cb.createQuery(Member.class);
+            Root<Member> m=query.from(Member.class);
+            CriteriaQuery<Member> cq=query.select(m).where(cb.equal(m.get("name"),"kim"));
+            List<Member> resultList=em.createQuery(cq).getResultList();
             tx.commit();
         }catch (Exception e){
             tx.rollback();
