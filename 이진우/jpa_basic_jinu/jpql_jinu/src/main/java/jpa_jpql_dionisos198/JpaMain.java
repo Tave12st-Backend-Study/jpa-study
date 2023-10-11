035e85bff5f31dce0b411c53d0645f06aca187c3
Jinu Yi <dionisos198@naver.com>
2023-10-11 12:19:30 +0900

    [Week5][Chap10] Feat: JPQL 명시적,묵시적 조인 (#59)

--- 이진우/jpa_basic_jinu/jpql_jinu/src/main/java/jpa_jpql_dionisos198/JpaMain.java
+++ 이진우/jpa_basic_jinu/jpql_jinu/src/main/java/jpa_jpql_dionisos198/JpaMain.java
@@ -2,6 +2,7 @@ package jpa_jpql_dionisos198;
 
 import javax.persistence.*;
 import java.lang.management.MemoryManagerMXBean;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -11,20 +12,26 @@ public class JpaMain {
         EntityTransaction tx=em.getTransaction();
         tx.begin();
         try{
+            Team team=new Team();
+            em.persist(team);
            Member member1=new Member();
            member1.setUsername("관리자1");
+           member1.setTeam(team);
            em.persist(member1);
 
            Member member2=new Member();
            member2.setUsername("관리자2");
+           member2.setTeam(team);
            em.persist(member2);
            em.flush();
            em.clear();
-         String query="select function('group_concat',m.username) From Member m";
-         List<String> result=em.createQuery(query,String.class).getResultList();
-            for (String s : result) {
-                System.out.println("s = "+s);
-            }
+
+           String query="select m.username from Member m";
+           String query2="select m.team from Member m";
+           String query3="select t.members from Team t";
+           String query4="select m from Team t join t.members m";
+           List<Member> result=em.createQuery(query4, Member.class).getResultList();
+            System.out.println(result);
             tx.commit();
         }catch (Exception e){
             tx.rollback();
