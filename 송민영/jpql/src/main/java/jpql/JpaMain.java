4b1038ea94e4be7198f0fd222cd880c56afb3969
siss-ankisile <ankisile@gmail.com>
2023-10-12 03:42:08 +0900

    [Week5][Chap10] Feat: 프로젝션 (#62)

---
+++ 송민영/jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,47 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
+
+            Member findMember = result.get(0);
+            findMember.setAge(20);
+
+            tx.commit();
+        }
+        catch (Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        }
+        finally {
+            em.close();
+        }
+        emf.close();
+
+    }
+
+
+
+}
