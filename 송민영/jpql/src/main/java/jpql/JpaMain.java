b9fd1e63abd2db54f49aefa5d2d6d1ee586a2c80
siss-ankisile <ankisile@gmail.com>
2023-10-12 04:24:46 +0900

    [Week5][Chap10] Feat: 기본 case식 (#62)

--- 송민영/jpql/src/main/java/jpql/JpaMain.java
+++ 송민영/jpql/src/main/java/jpql/JpaMain.java
@@ -30,13 +30,16 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m.username, 'HELLO', TRUE from Member m where  m.type = jpql.MemberType.ADMIN";
-            List<Object[]> resultList = em.createQuery( query).getResultList();
+            String query = "select " +
+                    "case when m.age <= 10 then '학생요금'" +
+                    "when m.age >=60 then '경로요금'" +
+                    "else '일반요금' " +
+                    "end" +
+                    "from Member m";
+            List<String> result = em.createQuery( query, String.class).getResultList();
 
-            for(Object[]  objects : resultList){
-                System.out.println("objects = " + objects[0]);
-                System.out.println("objects = " + objects[1]);
-                System.out.println("objects = " + objects[2]);
+            for(String s : result){
+                System.out.println("s = " + s);
             }
 
             tx.commit();
