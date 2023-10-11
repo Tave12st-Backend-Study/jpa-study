16b1a51ef28524523078aa1e639b9a4a99103bd3
jyjyjy25 <jyjyjy7418@gmail.com>
2023-10-11 21:27:35 +0900

    [Week5][Chap10] Feat: 소개 - 네이티브 SQL 기본 예제 (#61)

--- 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 김지윤/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -19,16 +19,8 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // Criteria 사용 준비
-            CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Member> query = cb.createQuery(Member.class);
-
-            // 루트 클래스 (조회를 시작할 클래스)
-            Root<Member> m = query.from(Member.class);
-
-            // 쿼리 생성
-            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("name"), "kim"));
-            List<Member> resultList = em.createQuery(cq).getResultList();
+            em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME from MEMBER")
+                            .getResultList();
 
             tx.commit();
         } catch (Exception e) {
