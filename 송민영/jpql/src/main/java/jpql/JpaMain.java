40907877dec1b205d0e8ecbca77a3963beb91fbd
siss-ankisile <ankisile@gmail.com>
2023-10-12 03:55:13 +0900

    [Week5][Chap10] Feat: 페이징 (#62)

--- 송민영/jpql/src/main/java/jpql/JpaMain.java
+++ 송민영/jpql/src/main/java/jpql/JpaMain.java
@@ -16,18 +16,27 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            for(int i=0;i<100;i++){
+                Member member = new Member();
+                member.setUsername("member"+i);
+                member.setAge(i);
+                em.persist(member);
+
+            }
 
             em.flush();
             em.clear();
 
-            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class).getResultList();
-            MemberDTO memberDTO = result.get(0);
-            System.out.println("memberDTO = " + memberDTO.getUsername());
-            System.out.println("memberDTO = " + memberDTO.getAge());
+            List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(10)
+                    .setMaxResults(20)
+                    .getResultList();
+
+            System.out.println("resultList = " + resultList.size());
+            for (Member member1 : resultList){
+                System.out.println("member1 = " + member1);
+            }
+
             tx.commit();
         }
         catch (Exception e){
