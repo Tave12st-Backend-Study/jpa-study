554dd2c5541f2f6071c285ddab358d6ad3876f36
siss-ankisile <ankisile@gmail.com>
2023-10-12 04:26:08 +0900

    [Week5][Chap10] Feat: coalesce (#62)

--- 송민영/jpql/src/main/java/jpql/JpaMain.java
+++ 송민영/jpql/src/main/java/jpql/JpaMain.java
@@ -30,12 +30,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select " +
-                    "case when m.age <= 10 then '학생요금'" +
-                    "when m.age >=60 then '경로요금'" +
-                    "else '일반요금' " +
-                    "end" +
-                    "from Member m";
+            String query = "select coalesce(m.username, '이름 없는 회원') from Member m";
             List<String> result = em.createQuery( query, String.class).getResultList();
 
             for(String s : result){
