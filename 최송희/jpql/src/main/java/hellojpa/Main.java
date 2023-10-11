c84be58673ae7c188e2bd95c08726c3cd326abce
songhee <96781855+songhee1@users.noreply.github.com>
2023-10-11 17:10:41 +0900

    [Week5][Chap10] Feat : 프로젝션
    프로젝션 종류 - 엔티티 프로젝션/엔티티 프로젝션/임베디드 타입 프로젝션/스칼라 프로젝션 + 여러 값을 프로젝션 하는 경우(Query, Object[], new생성자를 사용)

--- 최송희/jpql/src/main/java/hellojpa/Main.java
+++ 최송희/jpql/src/main/java/hellojpa/Main.java
@@ -16,17 +16,65 @@ public class Main {
 
                     Member member =  new Member();
                     member.setUsername("member1");
+                    member.setAge(10);
                     em.persist(member);
 
-                    //반환 타입이 명확할때 사용 :: TypedQuery<제네릭>
-                    TypedQuery<Member> query = em.createQuery("" +
-                            "SELECT m FROM Member m WHERE m.username = :username", Member.class);
-                    //파라미터 바인딩- 이름기준일때
-                    query.setParameter("username", "member1");
-                    Member singleResult = query.getSingleResult();
-                    System.out.println("singleResult = "+singleResult);
+                    em.flush();
+                    em.clear();
 
-                    //파라미터 바인딩-위치기준(추천 X)
+                    //1. 엔티티 프로젝션
+                    List<Member> result = em.createQuery(
+                            "SELECT m FROM Member m", Member.class
+                    ).getResultList();
+
+                    Member findMember = result.get(0);
+                    findMember.setAge(20);
+                    //UPDATE쿼리 발생..JPA에 의해 관리됨을 알 수 있다.
+
+
+                    //2. 엔티티 프로젝션
+                    List<Team> result2 = em.createQuery(
+                            "SELECT t FROM Member m JOIN m.team t", Team.class
+                    ).getResultList();
+                    //Member객체에 Team이 포함되어있어도 이렇게 해서 team과 조인하도록 작성 추천
+                    //쿼리 날아가는 것도 확인해보면 join됨을 알 수 있다.
+
+
+                    //3. 임베디드 타입 프로젝션
+                    em.createQuery(
+                            "SELECT o.address FROM Order o", Address.class
+                    ).getResultList();
+
+                    //4. 스칼라 타입 프로젝션
+                    //(1) Query타입으로 조회하는 경우
+                    List resultList = em.createQuery(
+                            "select m.username, m.age from Member m"
+                    ).getResultList();
+
+                    Object o = resultList.get(0);
+                    Object[] result3 = (Object[]) o;
+                    System.out.println("username = "+result3[0]);
+                    System.out.println("age = "+result3[1]);
+
+                    //(2) Object[] 타입으로 조회하는 경우
+                    List <Object[]> resultList1 = em.createQuery(
+                            "select m.username, m.age from Member m"
+                    ).getResultList();
+
+                    Object[] result4 = resultList1.get(0);
+                    System.out.println("username = "+result4[0]);
+                    System.out.println("age = "+result4[1]);
+
+                    //(3) new 명령어로 조회하는 경우
+                    List<MemberDTO> resultList2 = em.createQuery(
+                            "select new hellojpa.MemberDTO(m.username, m.age) from Member m"
+                            , MemberDTO.class
+                    ).getResultList();
+
+                    MemberDTO memberDTO = resultList2.get(0);
+                    System.out.println("username :: "+memberDTO.getUsername());
+
+
 
                     tx.commit();
 
