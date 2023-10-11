531bc7e76b7f165ea49dc69b35e4fa13598106c9
jyjyjy25 <jyjyjy7418@gmail.com>
2023-10-12 02:10:14 +0900

    [Week5][Chap10] Feat: JPQL 타입 표현과 기타식 - 엔티티 타입 표현 (#61)

--- 김지윤/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ 김지윤/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,9 +1,6 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.domain.Book;
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -24,6 +21,9 @@ public class JpaMain {
             book.setAuthor("김영한");
 
             em.persist(book);
+
+            em.createQuery("select i from Item i where type(i) = Book", Item.class)
+                            .getResultList();
 
             tx.commit();
         } catch (Exception e) {
