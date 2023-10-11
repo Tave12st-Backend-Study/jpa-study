0fd1711cbc59bd0867faf68f86b881dda86f8a4b
Jinu Yi <dionisos198@naver.com>
2023-10-11 12:49:44 +0900

    [Week5][chap11] Feat : FetchJOIN 기본 사용법 (#59)

--- 이진우/jpa_basic_jinu/jpql_jinu/src/main/java/jpa_jpql_dionisos198/JpaMain.java
+++ 이진우/jpa_basic_jinu/jpql_jinu/src/main/java/jpa_jpql_dionisos198/JpaMain.java
@@ -12,26 +12,54 @@ public class JpaMain {
         EntityTransaction tx=em.getTransaction();
         tx.begin();
         try{
-            Team team=new Team();
-            em.persist(team);
-           Member member1=new Member();
-           member1.setUsername("관리자1");
-           member1.setTeam(team);
+            Team teamA=new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB=new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+
+            Member member1=new Member();
+           member1.setUsername("회원1");
+           member1.setTeam(teamA);
            em.persist(member1);
 
            Member member2=new Member();
-           member2.setUsername("관리자2");
-           member2.setTeam(team);
+           member2.setUsername("회원2");
+           member2.setTeam(teamA);
            em.persist(member2);
+
+            Member member3=new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
+
+
+
            em.flush();
            em.clear();
 
-           String query="select m.username from Member m";
-           String query2="select m.team from Member m";
-           String query3="select t.members from Team t";
-           String query4="select m from Team t join t.members m";
-           List<Member> result=em.createQuery(query4, Member.class).getResultList();
-            System.out.println(result);
+          String query="select m from Member m";
+          String query2="select m from Member m join fetch m.team t";
+           List<Member> result=em.createQuery(query2, Member.class).getResultList();
+            for (Member member : result) {
+                System.out.println(member.getUsername()+"  "+member.getTeam().getName());
+            }
+            em.flush();
+            em.clear();;
+            System.out.println("================================");
+            String query3="select t from Team t";
+            String query4="select distinct t from Team t join fetch t.members m";
+            String query5="select t from Team t join t.members m";
+            List<Team> result2=em.createQuery(query5,Team.class).getResultList();
+            for (Team team : result2) {
+                System.out.println(team.getName()+" "+team.getMembers().size());
+                for(Member member9:team.getMembers()){
+                    System.out.println(member9.getUsername());
+                }
+            }
             tx.commit();
         }catch (Exception e){
             tx.rollback();
