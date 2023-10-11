6f7a4c734e17db0c26e84620ba4690efe46e28a1
jyjyjy25 <jyjyjy7418@gmail.com>
2023-10-11 22:17:10 +0900

    [Week5][Chap10] Feat: 기본 문법과 쿼리 API - jpql 프로젝트 셋팅 (#61)

---
+++ 김지윤/jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,32 @@
+package jpql;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+import java.util.Set;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
---
+++ 김지윤/jpql/src/main/java/jpql/Member.java
@@ -0,0 +1,40 @@
+package jpql;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
