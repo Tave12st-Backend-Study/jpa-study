72350b53e37c465462bc80f3a6044a1d1f13f7ed
songhee <96781855+songhee1@users.noreply.github.com>
2023-10-11 17:38:30 +0900

    [Week5][Chap10] Feat : 페이징 API

--- 최송희/jpql/src/main/java/hellojpa/Main.java
+++ 최송희/jpql/src/main/java/hellojpa/Main.java
@@ -14,67 +14,28 @@ public class Main {
                 tx.begin();
                 try{
 
-                    Member member =  new Member();
-                    member.setUsername("member1");
-                    member.setAge(10);
-                    em.persist(member);
+                    for(int i=0;i<100;i++) {
+                        Member member = new Member();
+                        member.setUsername("member"+(i+1));
+                        member.setAge(i);
+                        em.persist(member);
+                    }
 
                     em.flush();
                     em.clear();
 
-                    //1. 엔티티 프로젝션
-                    List<Member> result = em.createQuery(
-                            "SELECT m FROM Member m", Member.class
-                    ).getResultList();
 
-                    Member findMember = result.get(0);
-                    findMember.setAge(20);
-                    //UPDATE쿼리 발생..JPA에 의해 관리됨을 알 수 있다.
+                    List<Member> resultList2 = em.createQuery(
+                            "select m from Member m order by m.age desc"
+                            , Member.class
+                    ).setFirstResult(0)
+                    .setMaxResults(10)
+                    .getResultList();
 
-
-                    //2. 엔티티 프로젝션
-                    List<Team> result2 = em.createQuery(
-                            "SELECT t FROM Member m JOIN m.team t", Team.class
-                    ).getResultList();
-                    //Member객체에 Team이 포함되어있어도 이렇게 해서 team과 조인하도록 작성 추천
-                    //쿼리 날아가는 것도 확인해보면 join됨을 알 수 있다.
-
-
-                    //3. 임베디드 타입 프로젝션
-                    em.createQuery(
-                            "SELECT o.address FROM Order o", Address.class
-                    ).getResultList();
-
-                    //4. 스칼라 타입 프로젝션
-                    //(1) Query타입으로 조회하는 경우
-                    List resultList = em.createQuery(
-                            "select m.username, m.age from Member m"
-                    ).getResultList();
-
-                    Object o = resultList.get(0);
-                    Object[] result3 = (Object[]) o;
-                    System.out.println("username = "+result3[0]);
-                    System.out.println("age = "+result3[1]);
-
-                    //(2) Object[] 타입으로 조회하는 경우
-                    List <Object[]> resultList1 = em.createQuery(
-                            "select m.username, m.age from Member m"
-                    ).getResultList();
-
-                    Object[] result4 = resultList1.get(0);
-                    System.out.println("username = "+result4[0]);
-                    System.out.println("age = "+result4[1]);
-
-                    //(3) new 명령어로 조회하는 경우
-                    List<MemberDTO> resultList2 = em.createQuery(
-                            "select new hellojpa.MemberDTO(m.username, m.age) from Member m"
-                            , MemberDTO.class
-                    ).getResultList();
-
-                    MemberDTO memberDTO = resultList2.get(0);
-                    System.out.println("username :: "+memberDTO.getUsername());
-
-
+                    System.out.println("size : "+ resultList2.size());
+                    for(Member m : resultList2){
+                        System.out.println(m);
+                    }
 
                     tx.commit();
 
--- 최송희/jpql/src/main/java/hellojpa/Member.java
+++ 최송희/jpql/src/main/java/hellojpa/Member.java
@@ -36,4 +36,13 @@ public class Member {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
