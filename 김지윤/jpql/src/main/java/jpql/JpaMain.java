54114b86e04f00d993e43aa65361096e13ce776b
jyjyjy25 <jyjyjy7418@gmail.com>
2023-10-12 19:28:21 +0900

    [Week5][Chap10] Feat: JPQL 함수 (#61)

--- 김지윤/jpql/src/main/java/jpql/JpaMain.java
+++ 김지윤/jpql/src/main/java/jpql/JpaMain.java
@@ -18,53 +18,33 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
+            em.persist(member1);
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
-
-            member.setTeam(team);
-
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            // 기본 CASE
-            String query1 =
-                    "select " +
-                            "case when m.age <= 10 then '학생요금' " +
-                            "     when m.age >= 60 then '경로요금' " +
-                            "     else '일반요금' " +
-                            "end " +
-                    "from Member m";
-            List<String> resultList1 = em.createQuery(query1, String.class)
+            // JPQL 기본 함수 - concat
+            String query1 = "select concat('a', 'b') from Member m";
+            // JPQL 기본 함수 - substring
+            String query2 = "select substring(m.username, 2, 3) from Member m";
+            // JPQL 기본 함수 - locate
+            String query3 = "select locate('de', 'abcdefg') from Member m";
+            // JPQL 기본 함수 - size
+            String query4 = "select size(t.members) from Team t";
+            // 사용자 정의 함수 - group_concat
+            String query5 = "select function('group_concat', m.username) from Member m";
+            String query6 = "select group_concat(m.username) from Member m";
+
+            List<String> result = em.createQuery(query6, String.class)
                     .getResultList();
 
-            for (String s : resultList1) {
-                System.out.println("s = " + s);
-            }
-
-            // COALESCE
-            String query2 = "select coalesce(m.username, '이름 없는 회원') as username " +
-                    "from Member m";
-            List<String> resultList2 = em.createQuery(query2, String.class)
-                    .getResultList();
-
-            for (String s : resultList2) {
-                System.out.println("s = " + s);
-            }
-
-            // NULLIF
-            String query3 = "select nullif(m.username, '관리자') from Member m";
-            List<String> resultList3 = em.createQuery(query3, String.class)
-                    .getResultList();
-
-            for (String s : resultList3) {
+            for (String s : result) {
                 System.out.println("s = " + s);
             }
 
