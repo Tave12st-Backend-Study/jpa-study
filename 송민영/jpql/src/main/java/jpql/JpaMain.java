f7a80571b1ece8493fa4eb4aa4636d43521e471c
siss-ankisile <ankisile@gmail.com>
2023-10-12 23:07:52 +0900

    [Week5][Chap11] Feat: 일대다 fetch join (#62)

--- 송민영/jpql/src/main/java/jpql/JpaMain.java
+++ 송민영/jpql/src/main/java/jpql/JpaMain.java
@@ -42,11 +42,14 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m join fetch m.team";
-            List<Member> result = em.createQuery( query, Member.class).getResultList();
+            String query = "select distinct t from Team t join fetch t.members";
+            List<Team> result = em.createQuery( query, Team.class).getResultList();
 
-            for(Member member : result){
-                System.out.println("member = " + member.getUsername() + member.getTeam().getName());
+            for(Team team : result){
+                System.out.println("member = " + team.getName() + team.getMembers().size());
+                for(Member member : team.getMembers()){
+                    System.out.println("member = " + member);
+                }
             }
 
             tx.commit();
