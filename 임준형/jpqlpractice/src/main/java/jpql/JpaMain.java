2876b07d8e388f23576a69ab81018a566dcbcf0b
toychip <dlawnsgud427@naver.com>
2023-10-13 09:03:37 +0900

    [Week5][Chap10] Feat: 프로젝션 임베디드 타입과 한계 (#57)

--- 임준형/jpqlpractice/src/main/java/jpql/JpaMain.java
+++ 임준형/jpqlpractice/src/main/java/jpql/JpaMain.java
@@ -14,45 +14,18 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+//            em.flush();
+//            em.clear();
 
-            Member member = new Member();
-            member.setUsername("newnew");
-            member.setAge(10);
-            member.setTeam(team);
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-            
-            // 사용 xx 후에 경로 표현식에서 자세히 다룸
-            List<Team> resultList3 = em.createQuery("select m.team from Member m", Team.class)
+            // Order 안에 있는 임베디드 Adress 꺼내기가능, order에 의존적, 이것이 임베디드 타입의 한계
+            List<Order> resultList = em.createQuery("select o.address from Order o", Order.class)
                     .getResultList();
 
-            // 위는 예측이 불가능한 쿼리, 아래로 사용할 것
-            List<Team> resultList = em.createQuery("select m.team from Member m join m.team", Team.class)
-                    .getResultList();
+//            System.out.println("----- 쿼리 나가는 시점 -----");
+//            System.out.println("----- 쿼리 끝나는 시점 -----");
 
-
-
-            Team findTeam = resultList.get(0);
-            // join query가 나감
-            System.out.println("----- 쿼리 나가는 시점 -----");
-            System.out.println("findTeam.getName() = " + findTeam.getName());
-            System.out.println("----- 쿼리 끝나는 시점 -----");
-
-            System.out.println("----- 실제 쿼리 -----");
-            System.out.println("select\n" +
-                    "            team1_.id as id1_3_,\n" +
-                    "            team1_.name as name2_3_ \n" +
-                    "        from\n" +
-                    "            Member member0_ \n" +
-                    "        inner join\n" +
-                    "            Team team1_ \n" +
-                    "                on member0_.TEAM_ID=team1_.id");
-            System.out.println("----- 실제 쿼리 끝 -----");
+//            System.out.println("----- 실제 쿼리 -----");
+//            System.out.println("----- 실제 쿼리 끝 -----");
 
             tx.commit(); // 성공하면 커밋
 
