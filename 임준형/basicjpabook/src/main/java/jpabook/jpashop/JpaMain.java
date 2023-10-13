cae6b90f7b0fbd69bc5dc56e725df7cfe04b1a88
toychip <dlawnsgud427@naver.com>
2023-10-13 10:51:03 +0900

    [Week5][Chap10] Feat: 상속 관계일 때 엔티티 형태로 조회 (#57)

--- 임준형/basicjpabook/src/main/java/jpabook/jpashop/JpaMain.java
+++ 임준형/basicjpabook/src/main/java/jpabook/jpashop/JpaMain.java
@@ -3,11 +3,13 @@ package jpabook.jpashop;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -26,6 +28,25 @@ public class JpaMain {
             book.setAuthor("김영한");
 
             em.persist(book);
+
+            System.out.println("----- 상속 관계일 때 엔티티 형태로 확인 -----");
+            System.out.println("----- TYPE(m) = Member (상속 관계에서 사용) -----");
+            System.out.println("----- 쿼리 나가는 시점 -----");
+            List<Item> resultList = em.createQuery("select i from Item i where type(i) = Book", Item.class)
+                    .getResultList();
+            System.out.println("----- 쿼리 끝나는 시점-----\n");
+
+            System.out.println("\n----- 실제 쿼리 -----");
+            System.out.println("        select\n" +
+                    "            item0_.ITEM_ID as ITEM_ID2_3_,\n" +
+                    "            item0_.createBy as createBy3_3_,\n" +
+                    "            .... ,\n" +
+                    "            item0_.DTYPE as DTYPE1_3_ \n" +
+                    "        from\n" +
+                    "            Item item0_ \n" +
+                    "        where\n" +
+                    "            item0_.DTYPE='Book'");
+            System.out.println("----- 실제 쿼리 끝 -----");
 
             tx.commit(); // 성공하면 커밋
 
