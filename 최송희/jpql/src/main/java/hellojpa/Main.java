618d5d2fedfa5f276856c8f89a35cd0cff2b994a
songhee <96781855+songhee1@users.noreply.github.com>
2023-10-14 02:45:53 +0900

    [Week5][Chap10, 11] Feat : JPQL 추가적인 조사(#60)

--- 최송희/jpql/src/main/java/hellojpa/Main.java
+++ 최송희/jpql/src/main/java/hellojpa/Main.java
@@ -15,20 +15,11 @@ public class Main {
                 tx.begin();
                 try{
 
-                    Team team1 = new Team();
-                    team1.setName("Team1");
-                    em.persist(team1);
-
-                    Team team2 = new Team();
-                    team2.setName("Team2");
-                    em.persist(team2);
-
                     Member member1 = new Member();
                     member1.setUsername("member1");
                     member1.setAge(20);
                     member1.setMemberType(MemberType.ADMIN);
 
-                    member1.changeTeam(team1);
                     em.persist(member1);
 
                     Member member2 = new Member();
@@ -36,7 +27,6 @@ public class Main {
                     member2.setAge(20);
                     member2.setMemberType(MemberType.ADMIN);
 
-                    member2.changeTeam(team1);
                     em.persist(member2);
 
 
@@ -45,7 +35,6 @@ public class Main {
                     member3.setAge(20);
                     member3.setMemberType(MemberType.ADMIN);
 
-                    member3.changeTeam(team1);
                     em.persist(member3);
 
                     Member member4 = new Member();
@@ -53,23 +42,18 @@ public class Main {
                     member4.setAge(20);
                     member4.setMemberType(MemberType.ADMIN);
 
-                    member4.changeTeam(team2);
                     em.persist(member4);
 
-                    Member member5 = new Member();
-                    member5.setUsername("회원5");
-                    member5.setAge(20);
-                    member5.setMemberType(MemberType.ADMIN);
+                    int updateNUM = em.createQuery(
+                                    "UPDATE Member m SET m.username= 'songhee' where m.username = :name")
+                            .setParameter("name", "회원4")
+                            .executeUpdate();
 
-                    member5.changeTeam(team2);
-                    em.persist(member5);
+                    em.clear();
 
-                    String query = "select distinct t From Team t join fetch t.members";
-                    List<Team> resultList = em.createQuery(query, Team.class).getResultList();
 
-                    for(Object o : resultList){
-                        System.out.println(o);
-                    }
+                    Member findMember = em.find(Member.class, member4.getId());
+                    System.out.println("findMember name = " + findMember.getUsername());
 
                     tx.commit();
 
