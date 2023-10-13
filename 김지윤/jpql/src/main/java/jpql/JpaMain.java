f4a23985840e258cbd48e6ed79e65eadf3f93fc3
jyjyjy25 <jyjyjy7418@gmail.com>
2023-10-14 01:08:39 +0900

    [Week5][Chap11] Feat: 벌크 연산 (#61)

--- 김지윤/jpql/src/main/java/jpql/JpaMain.java
+++ 김지윤/jpql/src/main/java/jpql/JpaMain.java
@@ -29,29 +29,30 @@ public class JpaMain {
 
             Member member1 = new Member();
             member1.setUsername("회원1");
+            member1.setAge(0);
             member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("회원2");
+            member2.setAge(0);
             member2.setTeam(teamA);
             em.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("회원3");
+            member3.setAge(0);
             member3.setTeam(teamB);
             em.persist(member3);
 
-            em.flush();
-            em.clear();
+            // FLUSH 자동 호출
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
 
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
+            em.clear(); // 영속성 컨텍스트 초기화
 
-            for (Member member : resultList) {
-                System.out.println("member = " + member);
-            }
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findMember.getAge() = " + findMember.getAge());
 
             tx.commit();
         } catch (Exception e) {
