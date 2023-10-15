8b42d9b804049e99fc49a0683be46c680494aa7a
songhee <96781855+songhee1@users.noreply.github.com>
2023-10-16 01:28:28 +0900

    [Week6][Chap1] Feat : JPA와 db 설정, 동작확인(#71)

---
+++ 최송희/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+/*
+* Component 스캔 대상 -> 자동 스프링 빈 등록
+* */
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em; //스프링 부트에서는 엔티티 매니저를 생성 (spring-boot-data-jpa)
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+    /*
+    * command와 query를 분리해라 - id만 반환하도록
+    * */
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+
+}
---
+++ 최송희/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    /* Test 클래스 내부 @Transactional : Test 실행 후 DB ROLLBACK(DB 값 비는게 정상)*/
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then(검증)
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+
+    }
+}
