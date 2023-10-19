176d294eb75cd6af017e0a8990b6b2aad79853f5
jyjyjy25 <jyjyjy7418@gmail.com>
2023-10-19 23:35:10 +0900

    [Week6][Chap2] Feat: 엔티티 클래스 개발1 (#72)

---
+++ 김지윤/jpashop2/src/main/java/domain/Address.java
@@ -0,0 +1,15 @@
+package domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter @Setter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
