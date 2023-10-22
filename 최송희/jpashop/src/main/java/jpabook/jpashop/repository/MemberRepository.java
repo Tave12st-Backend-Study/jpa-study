310b201fbf547d5a66020849b0febd7566ad0e92
songhee1 <ghenrhkwk88@naver.com>
2023-10-23 00:01:55 +0900

    Week6][Chap4] Feat : 회원 리포지토리 개발(#71)

--- 최송희/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ 최송희/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -5,12 +5,14 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 import java.util.List;
 
 @Repository
+//컴포넌트 스캔의 대상이 된다
 public class MemberRepository {
     @PersistenceContext
-    private EntityManager em;
+    private EntityManager em; //스프링에서 em을 자동으로 주입
 
     public void save(Member member){
         em.persist(member);
@@ -22,6 +24,12 @@ public class MemberRepository {
 
     public List<Member> findAll(){
         return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where name = :name", Member.class)
+                .setParameter("name", name)
                 .getResultList();
     }
 }
