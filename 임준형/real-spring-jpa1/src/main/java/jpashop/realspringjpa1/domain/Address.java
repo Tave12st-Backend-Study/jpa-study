168278e5fba768d32dab21f9de72b57e9e37c3c1
toychip <dlawnsgud427@naver.com>
2023-10-22 23:47:32 +0900

    [Week6][Chap2] Feat: 엔티티 설계시 주의할 점 - @xxxToOne FetchType.Lazy 설정 (#70)

--- 임준형/real-spring-jpa1/src/main/java/jpashop/realspringjpa1/domain/Address.java
+++ 임준형/real-spring-jpa1/src/main/java/jpashop/realspringjpa1/domain/Address.java
@@ -1,15 +1,24 @@
 package jpashop.realspringjpa1.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 
 import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
