5ec66c9e07cf7fc30653bf403dd5afe551d028b7
jyjyjy25 <jyjyjy7418@gmail.com>
2023-10-23 19:49:21 +0900

    [Week7][Chap6] Feat: 주문, 주문상품 엔티티 개발 (#75)

--- 김지윤/jpashop2/src/main/java/jpabook/jpashop/domain/Order.java
+++ 김지윤/jpashop2/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.event.spi.DeleteEvent;
 
 import javax.persistence.*;
@@ -11,6 +13,7 @@ import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Table(name = "orders")
+@Getter @Setter
 public class Order {
 
     @Id @GeneratedValue
@@ -46,5 +49,45 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //* 생성 메서드 *//
+    public static Order crateOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //* 비즈니스 로직 *//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //* 조회 로직 *//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
