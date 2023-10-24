795b71dee7baa501fddb5537bb117bfd373c35ce
songhee1 <ghenrhkwk88@naver.com>
2023-10-25 01:17:47 +0900

    [Week6][Chap5] Feat : 상품 리포지토리, 서비스 개발(#74)

---
+++ 최송희/jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+    private final EntityManager em;
+
+    public void save(Item item){
+        if(item.getId() == null){
+            em.persist(item);
+            // id값 == null로 확인하는 이유 : 최초 저장시에는 id값이 null이기 때문에 이것을 이용한 것
+        }else em.merge(item); //업데이트의 의미
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll(){
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
---
+++ 최송희/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    //readOnly = false를 지정 :: 리포지토리 저장하도록 함(메서드 단위 @Transactional이 우선권을 가지기 때문)
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+
+}
