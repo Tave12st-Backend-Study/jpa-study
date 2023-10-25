b3b5ea46987a7a20e090311927f5a1fe2782b480
toychip <dlawnsgud427@naver.com>
2023-10-26 00:00:05 +0900

    [Week7][Chap7] Feat: 회원 등록 MemberController, MemberForm 추가 (#77)

---
+++ 임준형/real-spring-jpa1/src/main/java/jpashop/realspringjpa1/domain/controller/MemberController.java
@@ -0,0 +1,46 @@
+package jpashop.realspringjpa1.domain.controller;
+
+import javax.validation.Valid;
+import jpashop.realspringjpa1.domain.Address;
+import jpashop.realspringjpa1.domain.Member;
+import jpashop.realspringjpa1.domain.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Validated MemberForm form, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setUsername(form.getName());
+        member.setAddress(address);
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+}
