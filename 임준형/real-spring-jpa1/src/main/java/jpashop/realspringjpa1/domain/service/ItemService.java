2af9fa95e4fe692d0aff6c648f6b47f9f155c2f1
toychip <dlawnsgud427@naver.com>
2023-10-25 13:12:46 +0900

    [Week7[Chap5] Feat: 상품 서비스 개발 (#77)

---
+++ 임준형/real-spring-jpa1/src/main/java/jpashop/realspringjpa1/domain/service/ItemService.java
@@ -0,0 +1,29 @@
+package jpashop.realspringjpa1.domain.service;
+
+import java.util.List;
+import jpashop.realspringjpa1.domain.item.Item;
+import jpashop.realspringjpa1.domain.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
