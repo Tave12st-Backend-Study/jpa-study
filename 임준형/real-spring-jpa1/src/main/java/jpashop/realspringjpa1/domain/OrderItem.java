2d0b521365b85c5488d06c8ca480db8f6cf7c8fb
toychip <dlawnsgud427@naver.com>
2023-10-25 14:36:08 +0900

    [Week7][Chap6] Feat: [생성 메서드 활용] Order, OrderItem 엔티티 개발 (#77)

--- 임준형/real-spring-jpa1/src/main/java/jpashop/realspringjpa1/domain/OrderItem.java
+++ 임준형/real-spring-jpa1/src/main/java/jpashop/realspringjpa1/domain/OrderItem.java
@@ -27,4 +27,29 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
 
     private int count; // 주문 수량
+
+    // == 생성 메서드 == //
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setCount(count);
+
+        // 재고 줄이기
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    // == 비즈니스 로직 == //
+    /**
+     * 주문이 취소되었을 때 상품들의 수량이 다시 늘어나야함.
+     */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    // 한 아이템의 주문에 대해 총 주문 결과를 구하는 메서드
+    // 해당 주문 상품 전체 가격 조회 A book을 5개, B book을 2개 사면 각각 상품에 대해 구할때마다 메서드 호출됨
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
