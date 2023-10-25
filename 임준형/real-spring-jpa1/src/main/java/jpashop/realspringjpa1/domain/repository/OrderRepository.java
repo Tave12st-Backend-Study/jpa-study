45a4c6f1285de2503a4ecf0b733663af3781e0e1
toychip <dlawnsgud427@naver.com>
2023-10-25 17:21:27 +0900

    [Week7][Chap6] Feat: 주문 검색 기능 개발 (#77)

--- 임준형/real-spring-jpa1/src/main/java/jpashop/realspringjpa1/domain/repository/OrderRepository.java
+++ 임준형/real-spring-jpa1/src/main/java/jpashop/realspringjpa1/domain/repository/OrderRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import jpashop.realspringjpa1.domain.Order;
 import jpashop.realspringjpa1.domain.OrderStatus;
+import jpashop.realspringjpa1.domain.repository.dto.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -21,7 +22,19 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//        orderSearch.getOrderStatus == OrderStatus.ORDER
-//    }
+
+    /**
+     *
+     * 파라미터 값이 만약에 비어 있다면? -> queryDSL로 해결
+     *
+     */
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o From Order o join o.member m"
+                        + " where o.status = :status"
+                        + " and m.username like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)
+                .getResultList();
+    }
 }
