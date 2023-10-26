60cecb72c97b51577317763ae7238206f9187e22
Jinu Yi <dionisos198@naver.com>
2023-10-26 21:26:43 +0900

    [Week6][Chap1] Feat: Test 코드 작성 방법 (#79)

---
+++ 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/test/java/jpabook_jinu/jpashop_jinu/MemberRepositoryTest.java
@@ -0,0 +1,30 @@
+package jpabook_jinu.jpashop_jinu;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@SpringBootTest
+public class MemberRepositoryTest {
+     @Autowired MemberRepository memberRepository;
+
+     @Test
+    @Transactional
+    public void testMember(){
+         Member member=new Member();
+         member.setUsername("memberA");
+         Long saveId=memberRepository.save(member);
+
+         Member findMember=memberRepository.find(saveId);
+         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+         Assertions.assertThat(findMember).isEqualTo(member);
+
+
+     }
+}
