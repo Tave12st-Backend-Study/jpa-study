c0812366593012b789645ba3e416c13d9c886673
siss-ankisile <ankisile@gmail.com>
2023-10-27 02:46:43 +0900

    [Week7][Chap6] Feat: 주문, 주문상품 엔티티 개발 (#81)

--- 송민영/jpashop01/src/main/java/jpabook/jpashop01/domain/Order.java
+++ 송민영/jpashop01/src/main/java/jpabook/jpashop01/domain/Order.java
@@ -51,4 +51,43 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //==생성메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem : orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문취소
+     */
+
+    public void cancel(){
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem: orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 전체 주문 가격 조회
+     * @return
+     */
+    public int getTotalPrice(){
+        return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+    }
 }
--- 송민영/jpashop01/src/main/java/jpabook/jpashop01/domain/OrderItem.java
+++ 송민영/jpashop01/src/main/java/jpabook/jpashop01/domain/OrderItem.java
@@ -27,4 +27,24 @@ public class OrderItem {
     private int orderPrice; //주문가격
 
     private int count; //주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice()*getCount();
+    }
 }
