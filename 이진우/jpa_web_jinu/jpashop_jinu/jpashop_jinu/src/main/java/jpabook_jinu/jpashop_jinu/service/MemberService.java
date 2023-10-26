68dc0f53a8f8caa45f005ee3b2ff5a9a46592419
Jinu Yi <dionisos198@naver.com>
2023-10-27 02:30:48 +0900

    [Week6][Chap4] Feat: 회원 서비스 계층(필드 주입) (#79)

---
+++ 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/service/MemberService.java
@@ -0,0 +1,41 @@
+package jpabook_jinu.jpashop_jinu.service;
+
+import jpabook_jinu.jpashop_jinu.domain.Member;
+import jpabook_jinu.jpashop_jinu.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers=memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        }
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+
+
+}
+
