fa0cf020739714a3e9a9a37af9bcb84c155bd9e0
siss-ankisile <ankisile@gmail.com>
2023-10-26 22:12:06 +0900

    [Week6][Chap1] Feat: view 환경 설정 (#78)

--- 송민영/jpashop01/src/main/java/jpabook/jpashop01/Jpashop01Application.java
+++ 송민영/jpashop01/src/main/java/jpabook/jpashop01/Jpashop01Application.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Jpashop01Application {
 
     public static void main(String[] args) {
-        Hello hello = new Hello();
-        hello.setData("hello");
-        String data = hello.getData();
-        System.out.println("data = " + data);
         SpringApplication.run(Jpashop01Application.class, args);
     }
 
