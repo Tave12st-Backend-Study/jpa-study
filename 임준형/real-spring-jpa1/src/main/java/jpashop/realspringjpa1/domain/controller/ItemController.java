0a5c742405ea27e3c67416620fb5f66f853fd235
toychip <dlawnsgud427@naver.com>
2023-10-26 09:55:30 +0900

    [Week7][Chap7] Feat: 상품 수정 (#77)

--- 임준형/real-spring-jpa1/src/main/java/jpashop/realspringjpa1/domain/controller/ItemController.java
+++ 임준형/real-spring-jpa1/src/main/java/jpashop/realspringjpa1/domain/controller/ItemController.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -40,5 +42,37 @@ public class ItemController {
         List<Item> items = itemService.findItems();
         model.addAttribute("items", items);
         return "items/itemList";
+    }
+
+    @GetMapping("items/{itemId}/edit")
+    public String updateItemForm(@PathVariable Long itemId, Model model) {
+        Book book = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(book.getId());
+        form.setName(book.getName());
+        form.setPrice(book.getPrice());
+        form.setStockQuantity(book.getStockQuantity());
+        form.setAuthor(book.getAuthor());
+        form.setIsbn(book.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form) {
+
+        // TODO 검증 과정 필요
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
     }
 }
