31adbd06e4993894a6332400060217094feb500a
siss-ankisile <ankisile@gmail.com>
2023-10-27 03:07:55 +0900

    [Week7][Chap6] Feat: 주문 서비스 개발 (#81)

--- 송민영/jpashop01/src/main/java/jpabook/jpashop01/domain/OrderItem.java
+++ 송민영/jpashop01/src/main/java/jpabook/jpashop01/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop01.domain;
 
 import jpabook.jpashop01.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +11,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
---
+++ 송민영/jpashop01/src/main/java/jpabook/jpashop01/service/OrderService.java
@@ -0,0 +1,64 @@
+package jpabook.jpashop01.service;
+
+import jpabook.jpashop01.domain.Delivery;
+import jpabook.jpashop01.domain.Member;
+import jpabook.jpashop01.domain.Order;
+import jpabook.jpashop01.domain.OrderItem;
+import jpabook.jpashop01.domain.item.Item;
+import jpabook.jpashop01.repository.ItemRepository;
+import jpabook.jpashop01.repository.MemberRepository;
+import jpabook.jpashop01.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count){
+        //엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        //주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId){
+        //엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        //주문 취소
+        order.cancel();
+    }
+
+    /** 주문 검색 */
+/*
+ public List<Order> findOrders(OrderSearch orderSearch) {
+ return orderRepository.findAll(orderSearch);
+ }
+*/
+
+}
