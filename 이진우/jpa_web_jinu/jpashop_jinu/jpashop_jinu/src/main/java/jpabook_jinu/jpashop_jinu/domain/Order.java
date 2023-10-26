df80550d7f180055f21f7c67d6e3df2957f8c993
Jinu Yi <dionisos198@naver.com>
2023-10-27 00:18:44 +0900

    [Week6][Chap2] Feat:엔티티 클래스 개발 주의점-지연로딩 설정 (#79)

--- 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/domain/Order.java
+++ 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/domain/Order.java
@@ -16,7 +16,7 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
@@ -25,7 +25,7 @@ public class Order {
 
 
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
