82a7f14e37331735a8f2f7c52d59660f25ce3647
Jinu Yi <dionisos198@naver.com>
2023-10-27 02:41:57 +0900

    [Week6][Chap4] Feat: 회원가입 test (#79)

---
+++ 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/test/java/jpabook_jinu/jpashop_jinu/service/MemberServiceTest.java
@@ -0,0 +1,31 @@
+package jpabook_jinu.jpashop_jinu.service;
+
+import jpabook_jinu.jpashop_jinu.domain.Member;
+import jpabook_jinu.jpashop_jinu.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception{
+        //given
+        Member member=new Member();
+        member.setName("kim");
+
+        //when
+        Long saveId=memberService.join(member);
+
+        //then
+
+        Assertions.assertThat(member.getId()).isEqualTo(saveId);
+    }
+
+}
