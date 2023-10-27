9bdd33967769f767ed0608ea31bb9a598b1bb4e1
songhee1 <ghenrhkwk88@naver.com>
2023-10-27 22:39:42 +0900

    [Week6][Chap7] Feat : 회원가입(#74)

---
+++ 최송희/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Validated MemberForm form, BindingResult result){
+        if(result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/"; //첫 페이지로 이동
+    }
+}
