2a0acf1e8b71444a365ab82442e79d6b1bf6fc52
jaepyo99 <jaepyo99@naver.com>
2023-10-27 13:45:52 +0900

    [Week6][Chap1] Feat : init

--- 이재표/basic/jpql/src/main/java/hellojpa/jpql/JpaMain.java
+++ 이재표/basic/jpql/src/main/java/hellojpa/jpql/JpaMain.java
@@ -35,15 +35,14 @@ public class JpaMain {
             member3.setTeam(teamB);
             em.persist(member3);
 
-            em.flush();
-            em.clear();
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
-            for (Member member : resultList) {
-                System.out.println("member = "+member);
-            }
 
+
+            int resultCount = em.createQuery("update Member m set m.age=20").executeUpdate();
+            System.out.println("resultCount = "+resultCount);
+
+            em.clear();
+            Member member = em.find(Member.class, member1.getId());
+            System.out.println("memberAge "+member.getAge());
 
             tx.commit();
         } catch (Exception e) {
