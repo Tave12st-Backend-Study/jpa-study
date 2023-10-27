434ffa5c30f4580d311632aa42413c37ccd7a613
siss-ankisile <ankisile@gmail.com>
2023-10-27 11:42:55 +0900

    [Week7][Chap7] Feat: 회원 등록 (#81)

---
+++ 송민영/jpashop01/src/main/java/jpabook/jpashop01/controller/MemberController.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop01.controller;
+
+import jpabook.jpashop01.domain.Address;
+import jpabook.jpashop01.domain.Member;
+import jpabook.jpashop01.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result){
+        if(result.hasErrors()){
+            return "members/createMemberForm";
+        }
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+}
