7673c24efae79f6e97cfe34874739d2f9be6b107
Jinu Yi <dionisos198@naver.com>
2023-10-27 11:51:44 +0900

    [Week7][Chap6] Feat: 주문 리포지 토리 (#82)

---
+++ 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/repository/OrderRepository.java
@@ -0,0 +1,21 @@
+package jpabook_jinu.jpashop_jinu.repository;
+
+import jpabook_jinu.jpashop_jinu.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+    private final EntityManager em;
+
+    public void save(Order order){
+        em.persist(order);
+    }
+
+    public Order findOne(Long id){
+        return em.find(Order.class,id);
+    }
+}
