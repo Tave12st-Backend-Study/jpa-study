1dec161435e8d77ccba27d951acc5dc9a3202f72
siss-ankisile <ankisile@gmail.com>
2023-10-27 12:42:14 +0900

    [Week7][Chap7] Feat: 상품주문 (#81)

---
+++ 송민영/jpashop01/src/main/java/jpabook/jpashop01/controller/OrderController.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop01.controller;
+
+import jpabook.jpashop01.domain.Member;
+import jpabook.jpashop01.domain.item.Item;
+import jpabook.jpashop01.service.ItemService;
+import jpabook.jpashop01.service.MemberService;
+import jpabook.jpashop01.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model){
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count){
+        orderService.order(memberId,itemId,count);
+        return "redirect:/orders";
+    }
+}
