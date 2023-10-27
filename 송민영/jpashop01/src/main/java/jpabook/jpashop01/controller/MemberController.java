8bcad98bd73e4bf987b3b52545436a4096e03d76
siss-ankisile <ankisile@gmail.com>
2023-10-27 11:49:43 +0900

    [Week7][Chap7] Feat: 회원 목록 조회 (#81)

--- 송민영/jpashop01/src/main/java/jpabook/jpashop01/controller/MemberController.java
+++ 송민영/jpashop01/src/main/java/jpabook/jpashop01/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -37,4 +38,10 @@ public class MemberController {
         return "redirect:/";
     }
 
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "/members/memberList";
+    }
 }
