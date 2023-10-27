30b6d8a826a4792bf32bde59d9cb3e56a6ce9cc9
Jinu Yi <dionisos198@naver.com>
2023-10-27 11:58:51 +0900

    [Week7][Chap6] Feat: 주문 서비스 개발 (#82)

---
+++ 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/service/OrderService.java
@@ -0,0 +1,42 @@
+package jpabook_jinu.jpashop_jinu.service;
+
+import jpabook_jinu.jpashop_jinu.domain.*;
+import jpabook_jinu.jpashop_jinu.domain.item.Item;
+import jpabook_jinu.jpashop_jinu.repository.ItemRepository;
+import jpabook_jinu.jpashop_jinu.repository.MemberRepository;
+import jpabook_jinu.jpashop_jinu.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderService {
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+
+
+    @Transactional
+    public Long order(Long memberId,Long itemId,int count){
+        Member member=memberRepository.findOne(memberId);
+        Item item=itemRepository.findOne(itemId);
+
+        Delivery delivery=new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setDeliveryStatus(DeliveryStatus.READY);
+
+        OrderItem orderItem=OrderItem.createOrderItem(item,item.getPrice(),count);
+        Order order= Order.createOrder(member,delivery,orderItem);
+
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    @Transactional
+    public void cancelOrder(Long orderId){
+        Order order=orderRepository.findOne(orderId);
+        order.cancel();
+    }
+}
