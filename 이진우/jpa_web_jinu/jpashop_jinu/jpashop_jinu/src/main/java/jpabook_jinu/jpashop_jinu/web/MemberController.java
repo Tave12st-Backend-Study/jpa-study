c9e1048eae06f910bce50fd8ae805d227fc297e5
Jinu Yi <dionisos198@naver.com>
2023-10-27 17:46:23 +0900

    [Week7][Chap7] Feat: 회원목록 조회 (#82)

--- 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/web/MemberController.java
+++ 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/web/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -37,6 +38,13 @@ public class MemberController {
 
         return "redirect:/";
 
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model){
+        List<Member> members=memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 
 
