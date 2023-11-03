7f3fa6711a130953069ca34671d6ec00dab92262
songhee1 <ghenrhkwk88@naver.com>
2023-11-03 16:08:55 +0900

    [Week8][Chap3] Feat : 지연 로딩과 조회 성능 최적화 V1, V2(#93)

--- 최송희/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ 최송희/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -19,6 +19,7 @@ public class Order {
     @GeneratedValue
     @Column(name = "order_id")
     private Long id;
+
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
