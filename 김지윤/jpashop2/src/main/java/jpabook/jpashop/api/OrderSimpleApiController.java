fbf7a5227af78e44e657702e2a1fb455370ff906
jyjyjy25 <jyjyjy7418@gmail.com>
2023-11-06 03:57:45 +0900

    [Week8][Chap3] Feat: 간단한 주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화 (#94)

--- 김지윤/jpashop2/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ 김지윤/jpashop2/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -46,6 +46,16 @@ public class OrderSimpleApiController {
         return result;
     }
 
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
     @Data
     static class SimpleOrderDto {
         private Long orderId;
--- 김지윤/jpashop2/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ 김지윤/jpashop2/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -99,5 +99,12 @@ public class OrderRepository {
         return query.getResultList();
     }
 
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
+        ).getResultList();
+    }
 
 }
