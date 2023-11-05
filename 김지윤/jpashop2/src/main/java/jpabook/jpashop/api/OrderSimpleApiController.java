cc5bed21934eb2a2a8a6cc7a085174a243dc3b6c
jyjyjy25 <jyjyjy7418@gmail.com>
2023-11-06 04:05:54 +0900

    [Week8][Chap3] Feat: 간단한 주문 조회 V4: JPA에서 DTO로 바로 조회 (#94)

--- 김지윤/jpashop2/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ 김지윤/jpashop2/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +27,7 @@ import java.util.stream.Collectors;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
@@ -38,6 +41,8 @@ public class OrderSimpleApiController {
 
     @GetMapping("/api/v2/simple-orders")
     public List<SimpleOrderDto> ordersV2() {
+        // 총 ORDER 수 = 2개
+        //  N + 1 문제 -> 주문 1 + 회원 N + 배송 N
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
         List<SimpleOrderDto> result = orders.stream()
                 .map(o -> new SimpleOrderDto(o))
@@ -54,6 +59,11 @@ public class OrderSimpleApiController {
                 .collect(Collectors.toList());
 
         return result;
+    }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
     }
 
     @Data
--- 김지윤/jpashop2/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ 김지윤/jpashop2/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -106,5 +106,4 @@ public class OrderRepository {
                         " join fetch o.delivery d", Order.class
         ).getResultList();
     }
-
 }
