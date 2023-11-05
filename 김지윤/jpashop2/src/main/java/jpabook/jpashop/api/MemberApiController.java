d11f49f0301c5cd0efc1a4879c5f5ca03e2a7af1
jyjyjy25 <jyjyjy7418@gmail.com>
2023-11-06 03:36:08 +0900

    [Week8][Chap1] Feat: 회원 등록 API - v2 방식(엔티티 대신에 DTO를 RequestBody에 매핑) (#94)

--- 김지윤/jpashop2/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ 김지윤/jpashop2/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -25,6 +25,20 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+
     @Data
     static class CreateMemberResponse {
         private Long id;
