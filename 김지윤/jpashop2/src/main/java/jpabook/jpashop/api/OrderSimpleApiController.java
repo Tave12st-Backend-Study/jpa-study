fa96547e3f9696ccf19c6abca5f4bc5df9ed1751
jyjyjy25 <jyjyjy7418@gmail.com>
2023-11-06 03:56:06 +0900

    [Week8][Chap3] Feat: 간단한 주문 조회 V2: 엔티티를 DTO로 변환 (#94)

--- 김지윤/jpashop2/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ 김지윤/jpashop2/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -36,4 +36,31 @@ public class OrderSimpleApiController {
         return all;
     }
 
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+
 }
