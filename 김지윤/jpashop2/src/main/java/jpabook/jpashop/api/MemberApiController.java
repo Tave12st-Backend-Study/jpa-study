0e2159f189800ff6c74f47245ea6c08b35437ac4
jyjyjy25 <jyjyjy7418@gmail.com>
2023-11-06 03:42:16 +0900

    [Week8][Chap1] Feat: 회원 조회 API - v2 방식(응답 값으로 엔티티가 아닌 별도의 DTO 사용) (#94)

--- 김지윤/jpashop2/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ 김지윤/jpashop2/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -24,6 +24,28 @@ public class MemberApiController {
         return memberService.findMembers();
     }
 
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
         Long id = memberService.join(member);
@@ -60,8 +82,10 @@ public class MemberApiController {
         private Long id;
         private String name;
     }
+
     @Data
     static class CreateMemberRequest {
+        @NotEmpty
         private String name;
     }
 
