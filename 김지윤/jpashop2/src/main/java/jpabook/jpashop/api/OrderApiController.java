6ff989ecd70250f73ce33a2c4e143273bbac0262
jyjyjy25 <jyjyjy7418@gmail.com>
2023-11-06 13:47:33 +0900

    [Week8][Chap4] Feat: 주문 조회 V1: 엔티티 직접 노출 (#94)

---
+++ 김지윤/jpashop2/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> orderV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); // LAZY 강제 초기화
+            order.getDelivery().getAddress(); // LAZY 강제 초기화
+            List<OrderItem> orderItems = order.getOrderItems(); // LAZY 강제 초기화
+            orderItems.stream()
+                    .forEach(o -> o.getItem().getName()); // LAZY 강제 초기화
+        }
+        return all;
+    }
+}
