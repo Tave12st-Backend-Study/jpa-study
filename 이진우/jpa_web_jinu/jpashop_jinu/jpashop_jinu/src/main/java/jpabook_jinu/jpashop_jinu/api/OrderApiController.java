8d28801d922002a6c3f1a38e03553b7f7ecc2814
Jinu Yi <dionisos198@naver.com>
2023-11-08 15:25:39 +0900

    [Week8][Chap4] Feat: 컬렉션 - 주문 조회 V3 페치조인 최적화 (#95)

--- 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/api/OrderApiController.java
+++ 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/api/OrderApiController.java
@@ -41,6 +41,20 @@ public class OrderApiController {
 
     }
 
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3(){
+        List<Order> orders=orderRepository.findAllWithItem();
+
+        for (Order order : orders) {
+            System.out.println("order ref="+order+" id="+order.getId());
+        }
+        List<OrderDto> collect=orders.stream()
+                .map(o->new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+
+    }
+
 
     @Getter
     static class OrderDto{
