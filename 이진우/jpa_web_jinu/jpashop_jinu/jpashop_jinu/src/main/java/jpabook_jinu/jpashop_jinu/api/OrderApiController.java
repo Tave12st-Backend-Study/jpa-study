37bb6b372f221d86d93eae22598a39e93d942cdc
Jinu Yi <dionisos198@naver.com>
2023-11-08 12:29:36 +0900

    [Week8][Chap4] Feat: 컬렉션-주문 조최 V2 연관 엔티티 직접 조회 (#95)

--- 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/api/OrderApiController.java
+++ 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/api/OrderApiController.java
@@ -1,14 +1,16 @@
 package jpabook_jinu.jpashop_jinu.api;
 
-import jpabook_jinu.jpashop_jinu.domain.Order;
-import jpabook_jinu.jpashop_jinu.domain.OrderItem;
-import jpabook_jinu.jpashop_jinu.domain.OrderSearch;
+import jpabook_jinu.jpashop_jinu.domain.*;
 import jpabook_jinu.jpashop_jinu.repository.OrderRepository;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.cache.spi.support.AbstractCachedDomainDataAccess;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,5 +28,36 @@ public class OrderApiController {
             orderItems.stream().forEach(o->o.getItem().getName());
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2(){
+        List<Order> orders=orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect=orders.stream()
+                .map(o->new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+
+    }
+
+
+    @Getter
+    static class OrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItem> orderItems;
+        public OrderDto(Order order){
+            orderId=order.getId();
+            name=order.getMember().getName();
+            orderDate=order.getOrderDate();
+            orderStatus=order.getOrderStatus();
+            address=order.getDelivery().getAddress();
+            order.getOrderItems().stream().forEach(o->o.getItem().getName());
+            orderItems=order.getOrderItems();
+        }
+
     }
 }
