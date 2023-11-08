0c3f6ef2520f84b545905f79ae00b1cd976c5c51
Jinu Yi <dionisos198@naver.com>
2023-11-08 18:23:32 +0900

    [Week8][Chap4] Feat: 컬렉션 - 주문 조회 V5 컬렉션 조회 최적화 (#95)

--- 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/api/OrderApiController.java
+++ 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/api/OrderApiController.java
@@ -77,6 +77,11 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5(){
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
 
     @Getter
     static class OrderDto{
--- 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/repository/order/query/OrderQueryRepository.java
+++ 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/repository/order/query/OrderQueryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -36,6 +38,31 @@ public class OrderQueryRepository {
                        " from OrderItem oi"+
                        " join oi.item i"+
                        " where oi.order.id=:orderId", OrderItemQueryDto.class).setParameter("orderId",orderId).getResultList();
+
+   }
+
+   public List<OrderQueryDto> findAllByDto_optimization(){
+        List<OrderQueryDto> result=findOrders();
+        Map<Long,List<OrderItemQueryDto>> orderItemMap=
+                findOrderItemMap(toOrderIds(result));
+
+        result.forEach(o->o.setOrderItems(orderItemMap.get(o.getOrderId())));
+
+        return result;
+
+   }
+   private List<Long> toOrderIds(List<OrderQueryDto> result){
+        return result.stream().map(o->o.getOrderId()).collect(Collectors.toList());
+   }
+   private Map<Long,List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds){
+        List<OrderItemQueryDto> orderItems=em.createQuery(
+                "select new jpabook_jinu.jpashop_jinu.repository.order.query.OrderItemQueryDto(oi.order.id,i.name,oi.orderPrice,oi.count)"+
+                        " from OrderItem oi"+
+                        " join oi.item i" +
+                        " where oi.order.id in :orderIds", OrderItemQueryDto.class).setParameter("orderIds",orderIds)
+                .getResultList();
+
+        return orderItems.stream().collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
 
    }
 }
