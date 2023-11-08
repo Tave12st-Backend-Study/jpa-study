f8255fbd594c0da6674ed1f405c7693addee63a4
Jinu Yi <dionisos198@naver.com>
2023-11-08 16:04:08 +0900

    [Week8][Chap4] Feat: 컬렉션 - 주문 조회 V3.1 페이징 한계돌파 (#95)

--- 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/api/OrderApiController.java
+++ 이진우/jpa_web_jinu/jpashop_jinu/jpashop_jinu/src/main/java/jpabook_jinu/jpashop_jinu/api/OrderApiController.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.cache.spi.support.AbstractCachedDomainDataAccess;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -52,6 +54,18 @@ public class OrderApiController {
                 .map(o->new OrderDto(o))
                 .collect(Collectors.toList());
         return collect;
+
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(@RequestParam(value = "offset",defaultValue = "0") int offset, @RequestParam(value = "limit",defaultValue = "100")int limit){
+        List<Order> orders=orderRepository.findAllWithMemberDelivery(offset,limit);
+
+
+        List<OrderDto> result=orders.stream()
+                .map(o->new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
 
     }
 
