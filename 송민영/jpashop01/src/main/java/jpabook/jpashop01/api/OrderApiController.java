4da602bd6773a5fbdca1b2f9ecc81d5b857b8668
siss-ankisile <ankisile@gmail.com>
2023-11-13 01:21:33 +0900

    [Week8][Chap4] Feat: 주문조회 v3.1 (#96)

--- 송민영/jpashop01/src/main/java/jpabook/jpashop01/api/OrderApiController.java
+++ 송민영/jpashop01/src/main/java/jpabook/jpashop01/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.jpashop01.repository.OrderSearch;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -46,6 +47,19 @@ public class OrderApiController {
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3(){
         List<Order> orders = orderRepository.findAllWithItem();
+
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit
+    ){
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
 
         List<OrderDto> collect = orders.stream()
                 .map(o -> new OrderDto(o))
--- 송민영/jpashop01/src/main/java/jpabook/jpashop01/domain/Order.java
+++ 송민영/jpashop01/src/main/java/jpabook/jpashop01/domain/Order.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
