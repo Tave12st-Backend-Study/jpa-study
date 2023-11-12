38f45e12d3d5fc8b967bee0bea9cc2bfd19068d8
siss-ankisile <ankisile@gmail.com>
2023-11-13 00:24:33 +0900

    [Week8][Chap4] Feat: 주문조회 v3 (#96)

--- 송민영/jpashop01/src/main/java/jpabook/jpashop01/api/OrderApiController.java
+++ 송민영/jpashop01/src/main/java/jpabook/jpashop01/api/OrderApiController.java
@@ -43,6 +43,16 @@ public class OrderApiController {
         return collect;
     }
 
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3(){
+        List<Order> orders = orderRepository.findAllWithItem();
+
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
     @Getter
     static class OrderDto {
 
--- 송민영/jpashop01/src/main/java/jpabook/jpashop01/repository/OrderRepository.java
+++ 송민영/jpashop01/src/main/java/jpabook/jpashop01/repository/OrderRepository.java
@@ -100,4 +100,13 @@ public class OrderRepository {
     }
 
 
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
 }
