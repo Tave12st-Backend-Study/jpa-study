a2e307817d3502851f6454290dffe8cb4de51736
jaepyo99 <jaepyo99@naver.com>
2023-11-15 18:40:41 +0900

    [Week8][Chap4] Feat: 주문 조회V2(#96)

--- 이재표/spring-data-jpa1/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ 이재표/spring-data-jpa1/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -33,4 +33,47 @@ public class OrderApiController {
         }
         return all;
     }
+
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> orderV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+
+    @Data
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+        public OrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto{
+        private String itemName;
+        private int orderPrice;
+        private int count;
+        public OrderItemDto(OrderItem orderItem){
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
+
 }
