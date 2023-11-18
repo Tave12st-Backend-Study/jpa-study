decf36c60ddebc5f410da2625d1665ce5a8c1eb9
toychip <dlawnsgud427@naver.com>
2023-11-18 22:47:24 +0900

    [Week8][Chap1] feat: 회원 조회 API (#92)

---
+++ 임준형/real-spring-jpa2/src/main/java/jpashop/realspringjpa2/api/dto/MemberDto.java
@@ -0,0 +1,10 @@
+package jpashop.realspringjpa2.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MemberDto {
+    private String name;
+}
---
+++ 임준형/real-spring-jpa2/src/main/java/jpashop/realspringjpa2/api/dto/Result.java
@@ -0,0 +1,11 @@
+package jpashop.realspringjpa2.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Result<T> {
+    private int count;
+    private T data;
+}
