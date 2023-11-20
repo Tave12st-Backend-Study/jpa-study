04747ca32604fefe016d004ada7aeaa4417f1df0
songhee1 <ghenrhkwk88@naver.com>
2023-11-20 22:44:16 +0900

    [Week9][Chap4] Feat : 파라미터 바인딩 (#107)

--- 최송희/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 최송희/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -10,6 +10,7 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -121,6 +122,18 @@ class MemberRepositoryTest {
         List<MemberDto> memberDto = memberRepository.findMemberDto();
         for (MemberDto s : memberDto) {
             System.out.println("dto = "+s);
+        }
+
+    }
+
+    @Test
+    public void findByNames(){
+        Member m1 = new Member("AAA", 10);
+        memberRepository.save(m1);
+
+        List<Member> byNames = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        for (Member byName : byNames) {
+            System.out.println(byName);
         }
 
     }
