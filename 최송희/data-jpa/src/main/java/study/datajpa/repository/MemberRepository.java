c72a592f07da1daa518951320040f6cfb4dfbae4
songhee1 <ghenrhkwk88@naver.com>
2023-11-20 22:12:07 +0900

    [Week9][Chap4] Feat : 리포지토리에 쿼리정의(@Query) (#107)

--- 최송희/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ 최송희/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -8,4 +10,7 @@ import java.util.List;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsername(String username);
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
--- 최송희/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 최송희/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -77,4 +77,17 @@ class MemberRepositoryTest {
         assertThat(result.size()).isEqualTo(1);
 
     }
+
+    @Test
+    public void testQuery(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+        assertThat(result.get(0)).isEqualTo(m1);
+
+    }
 }
