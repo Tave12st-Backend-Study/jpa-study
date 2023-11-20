e030e314b04c0c2b337d9fb489668a23d0bce3e5
Jinu Yi <dionisos198@naver.com>
2023-11-20 20:31:10 +0900

    [Week9][Chap3] Feat : 공통 인터페이스 적용 (#108)

--- 이진우/spring_data_jpa_jinu/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 이진우/spring_data_jpa_jinu/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Transactional
@@ -33,5 +35,30 @@ class MemberRepositoryTest {
 
 
     }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        Assertions.assertThat(findMember1).isEqualTo(member1);
+        Assertions.assertThat(findMember2).isEqualTo(member2);
+
+        List<Member> all = memberRepository.findAll();
+        Assertions.assertThat(all.size()).isEqualTo(2);
+
+        long count = memberRepository.count();
+        Assertions.assertThat(count).isEqualTo(2);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        long deletedCount = memberRepository.count();
+        Assertions.assertThat(deletedCount).isEqualTo(0);
+    }
+
 
 }
