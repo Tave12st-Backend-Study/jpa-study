0abe56790bece80e35c8a1f038707c420effbd52
songhee1 <ghenrhkwk88@naver.com>
2023-11-20 17:47:03 +0900

    [Week9][Chap1] Feat : 엔티티와 레포지토리, 테스트 클래스 코드 작성(#107)

---
+++ 최송희/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -0,0 +1,22 @@
+package study.datajpa.repository;
+
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberJpaRepository {
+    @PersistenceContext //엔티티 매니저를 가져와서, JPA가 알아서 DB와 주고받을 수 있도록 한다.
+    private EntityManager em;
+
+    public Member save(Member member){
+        em.persist(member);
+        return member;
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}
