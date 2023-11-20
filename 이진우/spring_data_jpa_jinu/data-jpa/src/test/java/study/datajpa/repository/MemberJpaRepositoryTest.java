3e9ee7f20afce70f1cc775f8e80e32b6d6846b0c
Jinu Yi <dionisos198@naver.com>
2023-11-20 19:31:22 +0900

    [Week9][Chap1] Feat : 프로젝트 세팅 및 테스트 (#108)

---
+++ 이진우/spring_data_jpa_jinu/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,36 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember(){
+        Member member=new Member("memberA");
+        Member savedMember=memberJpaRepository.save(member);
+
+        Member findMember=memberJpaRepository.find(savedMember.getId());
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+
+
+    }
+
+}
---
+++ 이진우/spring_data_jpa_jinu/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,37 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberRepositoryTest {
+
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember(){
+        Member member=new Member("memberA");
+        Member savedMember=memberRepository.save(member);
+
+        Member findMember=memberRepository.findById(savedMember.getId()).get();
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+
+
+    }
+
+}
