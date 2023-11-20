8dda937905e9cdf8287ea61195a314eb84677a6a
songhee1 <ghenrhkwk88@naver.com>
2023-11-21 00:24:10 +0900

    [Week9][Chap4] Feat : 순수 JPA페이징과 정렬 (#107)

--- 최송희/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ 최송희/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -47,4 +47,19 @@ public class MemberJpaRepository {
                 .setParameter("age", age)
                 .getResultList();
     }
+
+    //페이징
+    public List<Member> findByPage(int age, int offset, int limit){
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc ", Member.class)
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long totalCount(int age){
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 }
--- 최송희/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ 최송희/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -78,4 +78,31 @@ class MemberJpaRepositoryTest {
         assertThat(result.size()).isEqualTo(1);
 
     }
+
+    @Test
+    public void findByPage(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 10);
+        Member m3 = new Member("BBB", 10);
+        Member m4 = new Member("BBB", 10);
+        Member m5 = new Member("BBB", 10);
+
+        memberJpaRepository.save(m1);
+        memberJpaRepository.save(m2);
+        memberJpaRepository.save(m3);
+        memberJpaRepository.save(m4);
+        memberJpaRepository.save(m5);
+
+        //page1 offset = 0, limit = 15, page2 offset = 15 ....
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+        List<Member> members = memberJpaRepository.findByPage(age, offset, limit);
+        long totalCount = memberJpaRepository.totalCount(age);
+
+        assertThat(members.size()).isEqualTo(3);
+        assertThat(totalCount).isEqualTo(5);
+
+    }
 }
