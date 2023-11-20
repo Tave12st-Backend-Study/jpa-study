1cc57683da035e874b154d6f6e6d2849b296dc31
songhee1 <ghenrhkwk88@naver.com>
2023-11-20 21:09:10 +0900

    [Week9][Chap3] Feat : 공통 인터페이스 분석(#107)

--- 최송희/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ 최송희/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import java.util.List;
 
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsername();
 }
