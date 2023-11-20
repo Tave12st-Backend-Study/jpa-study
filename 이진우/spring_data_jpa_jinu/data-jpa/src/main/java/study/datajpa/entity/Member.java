c0f52d4b475ec0415f865d0d7389cb2208048001
Jinu Yi <dionisos198@naver.com>
2023-11-20 19:55:04 +0900

    [Week9][Chap2] Feat : 도메인 세팅 (#108)

--- 이진우/spring_data_jpa_jinu/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ 이진우/spring_data_jpa_jinu/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,25 +1,48 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","username","age"})
 public class Member {
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String username;
 
-    protected Member(){
+    private int age;
 
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+
 
     public Member(String username) {
-        this.username = username;
+        this.username=username;
+    }
+
+    public Member(String username,int age){
+        this.username=username;
+        this.age=age;
+    }
+
+    public Member(String username,int age,Team team){
+        this.username=username;
+        this.age=age;
+
+        if(team!=null){
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team){
+        this.team=team;
+        team.getMembers().add(this);
     }
 }
---
+++ 이진우/spring_data_jpa_jinu/data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -0,0 +1,50 @@
+package study.datajpa.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+
+class MemberTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+
+        List<Member> members = em.createQuery("select m from Member m",
+                        Member.class)
+                .getResultList();
+        for (Member member : members) {
+            System.out.println("member=" + member);
+            System.out.println("-> member.team=" + member.getTeam());
+        }
+    }
+}
+
