f32ea275cbcada3a49bfd083fc4b70bf0c8024ff
Jinu Yi <dionisos198@naver.com>
2023-11-20 21:44:37 +0900

    [Week9][Chap4] Feat : 컬렉션 파라미터 바인딩 in 절 사용 (#108)

--- 이진우/spring_data_jpa_jinu/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 이진우/spring_data_jpa_jinu/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
 import java.lang.management.MemoryManagerMXBean;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -124,6 +125,19 @@ class MemberRepositoryTest {
 
         for (MemberDto dto : memberDto) {
             System.out.println("dto = "+dto);
+        }
+    }
+
+    @Test
+    public void findByNames(){
+        Member m1=new Member("AAA",10);
+        Member m2= new Member("BBB",20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA","BBB"));
+        for (Member member : result) {
+            System.out.println("member = "+ member);
         }
     }
 
