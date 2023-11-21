800379201ff28a1f29bae28290b4c29d1f275c13
jyjyjy25 <jyjyjy7418@gmail.com>
2023-11-22 01:59:49 +0900

    [Week9][Chap4] Feat: 순수 JPA 페이징과 정렬 (#106)

--- 김지윤/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ 김지윤/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -87,4 +87,30 @@ class MemberJpaRepositoryTest {
         Member findMember = result.get(0);
         assertThat(findMember).isEqualTo(m1);
     }
+
+    @Test
+    public void paging() {
+        // given
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 10));
+        memberJpaRepository.save(new Member("member3", 10));
+        memberJpaRepository.save(new Member("member4", 10));
+        memberJpaRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+
+        // when
+        List<Member> members = memberJpaRepository.findByPage(age, offset, limit);
+        long totalCount = memberJpaRepository.totalCount(age);
+
+        // 페이지 계산 공식 적용
+        // totalPage = totalCount / size
+
+        // then
+        assertThat(members.size()).isEqualTo(3);
+        assertThat(totalCount).isEqualTo(5);
+
+    }
 }
