48e3f29babded40498c77802e9989c332ea8f947
toychip <dlawnsgud427@naver.com>
2023-11-21 23:11:08 +0900

    [Week9][Chap3] feat: 공통 인터페이스 설정, 적용, 분석 (#105)

--- 임준형/SpringDataJpa/src/main/java/study/SpringDataJpa/repository/TeamRepository.java
+++ 임준형/SpringDataJpa/src/main/java/study/SpringDataJpa/repository/TeamRepository.java
@@ -1,43 +1,7 @@
 package study.SpringDataJpa.repository;
 
-import java.util.List;
-import java.util.Optional;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import study.SpringDataJpa.entity.Team;
 
-@Repository
-public class TeamRepository {
-
-    @PersistenceContext
-    EntityManager em;
-
-    public Team saveTeam(Team team) {
-        em.persist(team);
-        return team;
-    }
-
-    public void delete(Team team) {
-        em.remove(team);
-    }
-
-    public List<Team> findAll() {
-        return em.createQuery("select t from Team t", Team.class)
-                .getResultList();
-    }
-
-    public Optional<Team> findById(Long id) {
-        Team team = em.find(Team.class, id);
-        return Optional.ofNullable(team);
-    }
-
-    public long count() {
-        return em.createQuery("select count(t) from Team t", Long.class)
-                .getSingleResult();
-    }
-
-    public Team find(Long id) {
-        return em.find(Team.class, id);
-    }
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
