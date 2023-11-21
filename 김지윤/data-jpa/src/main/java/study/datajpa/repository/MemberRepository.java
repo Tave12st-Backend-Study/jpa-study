5a9902207df0fdb53f26917bcca13bc1ab5ac6f4
jyjyjy25 <jyjyjy7418@gmail.com>
2023-11-22 01:38:36 +0900

    [Week9][Chap4] Feat: 반환 타입 (#106)

--- 김지윤/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ 김지윤/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -8,6 +8,7 @@ import study.datajpa.entity.Member;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -28,4 +29,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.username in :names")
     List<Member> findByNames(@Param("names") Collection<String> names);
+
+    List<Member> findListByUsername(String username); // 컬렉션
+    Member findMemberByUsername(String username); // 단건
+    Optional<Member> findOptionalByUsername(String username); // 단건 Optional
 }
--- 김지윤/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 김지윤/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import study.datajpa.entity.Team;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -147,5 +148,23 @@ class MemberRepositoryTest {
         for (Member member : result) {
             System.out.println("member = " + member);
         }
+    }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> memberList = memberRepository.findListByUsername("AAA");
+        System.out.println("memberList = " + memberList);
+        System.out.println("================");
+        Member member = memberRepository.findMemberByUsername("AAA");
+        System.out.println("member = " + member);
+        System.out.println("================");
+        Optional<Member> optionalMember = memberRepository.findOptionalByUsername("AAA");
+        System.out.println("optionalMember = " + optionalMember);
+
     }
 }
