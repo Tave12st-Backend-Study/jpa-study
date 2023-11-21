7ea6bc74e1cd827ae8a2aeefa4797f353bfc892f
toychip <dlawnsgud427@naver.com>
2023-11-21 23:10:30 +0900

    [Week9][Chap3] feat: 공통 인터페이스 설정, 적용, 분석 (#105)

--- 임준형/SpringDataJpa/src/test/java/study/SpringDataJpa/repository/MemberRepositoryTest.java
+++ 임준형/SpringDataJpa/src/test/java/study/SpringDataJpa/repository/MemberRepositoryTest.java
@@ -1,7 +1,9 @@
 package study.SpringDataJpa.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -18,24 +20,66 @@ import study.SpringDataJpa.entity.Team;
 class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
+    @Autowired TeamRepository teamRepository;
 
     @Test
     public void testMember() {
-        Team teamA = Team.builder()
+
+        Team team = Team.builder()
                 .name("teamA")
                 .build();
+        teamRepository.save(team);
 
         String username = "memberA";
         Member member = Member.builder()
+                .age(20)
                 .username(username)
-                .team(teamA)
+                .team(team)
                 .build();
 
         Member savedMember = memberRepository.save(member);
+
         Member findMember = memberRepository.findById(savedMember.getId()).get();
 
         assertEquals(savedMember.getId(), findMember.getId());
         assertEquals(savedMember.getUsername(), findMember.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member);
+        assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    void basicCrud() {
+        Member member1 = Member.builder()
+                .username("teamA")
+                .age(13)
+                .build();
+
+        Member member2 = Member.builder()
+                .username("teamA")
+                .age(13)
+                .build();
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+
+        // 리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 }
