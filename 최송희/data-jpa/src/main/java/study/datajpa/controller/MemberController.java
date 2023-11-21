726a79d69afcc0cd02274f8506ee72e2d5c18a55
songhee1 <ghenrhkwk88@naver.com>
2023-11-21 16:53:16 +0900

    [Week9][Chap5] Feat : web확장- 도메인 클래스 컨버터(#107)

---
+++ 최송희/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,33 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController
+{
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id){
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members/{id}")
+    public String findMember2(@PathVariable("id") Member member){
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init(){
+        memberRepository.save(new Member("userA"));
+    }
+}
