aee0732da625cfe35db83f5a40f56f036726a742
jyjyjy25 <jyjyjy7418@gmail.com>
2023-11-22 02:43:20 +0900

    [Week9][Chap4] Feat: 스프링 데이터 JPA 페이징과 정렬 - Slice (#106)

--- 김지윤/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ 김지윤/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package study.datajpa.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -36,6 +37,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findMemberByUsername(String username); // 단건
     Optional<Member> findOptionalByUsername(String username); // 단건 Optional
 
-    Page<Member> findByAge(int age, Pageable pageable);
+    Slice<Member> findByAge(int age, Pageable pageable);
 
 }
