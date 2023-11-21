f82080001aeec774c9825bd73c356c2bdb8d3538
jyjyjy25 <jyjyjy7418@gmail.com>
2023-11-21 22:48:42 +0900

    [Week9][Chap3] Feat: 공통 인터페이스 설정 (#106)

--- 김지윤/data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
+++ 김지윤/data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,6 +2,7 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class DataJpaApplication {
--- 김지윤/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 김지윤/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -18,6 +18,7 @@ class MemberRepositoryTest {
 
     @Test
     public void testMember() {
+        System.out.println("memberRepository = " + memberRepository.getClass());
         Member member = new Member("memberA");
         Member savedMember = memberRepository.save(member);
 
