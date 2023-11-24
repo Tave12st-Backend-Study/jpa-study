51f0d3a63b30d09a58a743157f58279de34b47d5
songhee1 <ghenrhkwk88@naver.com>
2023-11-24 21:32:29 +0900

    [Week10][Chap1] Feat : 스프링부트 설정-JPA, DB(#111)

--- 최송희/querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ 최송희/querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
@@ -14,6 +15,7 @@ import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
 	@Autowired
