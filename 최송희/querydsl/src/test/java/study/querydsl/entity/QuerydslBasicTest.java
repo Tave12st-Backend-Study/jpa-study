24f53af4cdcf9191edc23e2dee5543d590961129
songhee1 <ghenrhkwk88@naver.com>
2023-11-24 23:15:25 +0900

    [Week10][Chap3] Feat : 정렬(#111)

--- 최송희/querydsl/src/test/java/study/querydsl/entity/QuerydslBasicTest.java
+++ 최송희/querydsl/src/test/java/study/querydsl/entity/QuerydslBasicTest.java
@@ -132,4 +132,33 @@ public class QuerydslBasicTest {
                 .selectFrom(member)
                 .fetchCount();
     }
+
+    /*
+    * 회원정렬순서
+    * 1.회원 나이 내림차순
+    * 2. 회원 이름 오름차순
+    * 단 2에서 회원 이름이 없으면 마지막에 출력(null last)
+    * */
+    @Test
+    public void sort(){
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(),
+                        member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member membernull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(membernull.getUsername()).isNull();
+
+    }
 }
