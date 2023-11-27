23bd2ba169f55eddeab029f16051d454dc0f1e88
toychip <dlawnsgud427@naver.com>
2023-11-27 13:35:27 +0900

    [Week9][Chap5] feat: Web 확장 - 페이징과 정렬 (#105)

--- 임준형/SpringDataJpa/src/main/java/study/SpringDataJpa/controller/MemberController.java
+++ 임준형/SpringDataJpa/src/main/java/study/SpringDataJpa/controller/MemberController.java
@@ -1,12 +1,17 @@
 package study.SpringDataJpa.controller;
 
+import java.util.List;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import study.SpringDataJpa.entity.Member;
 import study.SpringDataJpa.repository.MemberRepository;
+import study.SpringDataJpa.repository.dto.MemberDto;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,12 +32,29 @@ public class MemberController {
         return member.getUsername();
     }
 
+//    @GetMapping("/members")
+//    public Page<Member> list(
+//            @PageableDefault(size = 5, sort = "username") Pageable pageable) {
+//        return memberRepository.findAll(pageable);
+//    }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(
+            @PageableDefault(size = 5, sort = "username") Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+//        return page.map(MemberDto::new); 둘 다 가능
+        return page.map(MemberDto::toMemberDto);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(Member.builder()
-                .username("userA")
-                .age(10)
-                .team(null)
-                .build());
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(Member.builder()
+                    .username("user" + i)
+                    .age(i)
+                    .team(null)
+                    .build());
+        }
     }
+
 }
--- 임준형/SpringDataJpa/src/main/java/study/SpringDataJpa/repository/dto/MemberDto.java
+++ 임준형/SpringDataJpa/src/main/java/study/SpringDataJpa/repository/dto/MemberDto.java
@@ -2,6 +2,7 @@ package study.SpringDataJpa.repository.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import study.SpringDataJpa.entity.Member;
 
 @Data
 public class MemberDto {
@@ -14,5 +15,16 @@ public class MemberDto {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
+    }
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
+        this.teamName = "teamA";
+    }
+
+    public static MemberDto toMemberDto(Member member) {
+        return new MemberDto(
+                member.getId(), member.getUsername(), "team");
     }
 }
