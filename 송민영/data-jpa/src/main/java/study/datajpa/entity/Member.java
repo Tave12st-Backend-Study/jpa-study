8568abf2138d98de2eaf536ee8394663ac391b2a
siss-ankisile <ankisile@gmail.com>
2023-12-01 23:53:26 +0900

    [Week9][Chap4] Feat: 쿼리 메서드(#115)

--- 송민영/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ 송민영/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -26,6 +26,11 @@ public class Member {
         this.username = username;
     }
 
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
     public Member(String username, int age, Team team) {
         this.username = username;
         this.age = age;
--- 송민영/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 송민영/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -57,4 +57,18 @@ class MemberRepositoryTest {
         assertThat(deletedCount).isEqualTo(0);
     }
 
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+        List<Member> result =
+                memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+
 }
