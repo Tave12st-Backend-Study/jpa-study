142fbba5cf8bff330394923b9a32369642b7c39b
siss-ankisile <ankisile@gmail.com>
2023-12-02 00:22:03 +0900

    [Week9][Chap4] Feat: 파라미터 바인딩 (#115)

--- 송민영/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 송민영/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -109,6 +110,20 @@ class MemberRepositoryTest {
         List<MemberDto> memberDto = memberRepository.findMemberDto();
         for(MemberDto dto : memberDto) {
             System.out.println("dto = " + dto);
+        }
+    }
+
+    @Test
+    public void findByNames() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        for(Member member : result){
+            System.out.println("member = " + member);
         }
     }
 
