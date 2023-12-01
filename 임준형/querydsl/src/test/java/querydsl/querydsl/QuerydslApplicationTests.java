fcadc3d70bd2691e70309e80fec2ff804e2b808a
toychip <dlawnsgud427@naver.com>
2023-12-01 14:31:25 +0900

    [Week10][Chap1] setting: queryDSL 설정과 검증 (#113)

--- 임준형/querydsl/src/test/java/querydsl/querydsl/QuerydslApplicationTests.java
+++ 임준형/querydsl/src/test/java/querydsl/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,39 @@
 package querydsl.querydsl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import querydsl.querydsl.domain.QTestEntity;
+import querydsl.querydsl.domain.TestEntity;
 
 @SpringBootTest
 class QuerydslApplicationTests {
 
+	@Autowired
+	EntityManager em;
+
 	@Test
+	@Transactional
 	void contextLoads() {
+		TestEntity testEntity = new TestEntity();
+		em.persist(testEntity);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+//		QTestEntity qTestEntity = new QTestEntity("testEntityt");
+		QTestEntity qTestEntity = QTestEntity.testEntity;
+
+		TestEntity result = query
+				.selectFrom(qTestEntity)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(testEntity);
+		assertThat(result.getId()).isEqualTo(testEntity.getId());
 	}
 
 }
