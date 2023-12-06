b26fe11d25b7a159e348ccc067ab866c8e648ea9
toychip <dlawnsgud427@naver.com>
2023-12-06 11:07:05 +0900

    [Week10][Chap6] feat: 스프링 데이터 페이징 활용3 - 컨트롤러 개발 (#113)
    페이징의 끝판왕을 Api로 변경 후 테스트

--- 임준형/querydsl/src/main/java/querydsl/querydsl/controller/MemberController.java
+++ 임준형/querydsl/src/main/java/querydsl/querydsl/controller/MemberController.java
@@ -2,10 +2,13 @@ package querydsl.querydsl.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import querydsl.querydsl.dto.MemberSearchCondition;
 import querydsl.querydsl.dto.MemberTeamDto;
+import querydsl.querydsl.repository.MemberRepository;
 import querydsl.querydsl.repository.PreMemberJpaRepository;
 
 @RestController
@@ -13,9 +16,15 @@ import querydsl.querydsl.repository.PreMemberJpaRepository;
 public class MemberController {
 
     private final PreMemberJpaRepository preMemberJpaRepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping("v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return preMemberJpaRepository.search(condition);
+    }
+
+    @GetMapping("v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageComplex(condition, pageable);
     }
 }
